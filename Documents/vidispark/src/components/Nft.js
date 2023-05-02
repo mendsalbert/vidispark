cab95b41a6f1fe641c1bbfcd7e910468b8aaa3dc
mendsalbert <mendsalbert@gmail.com>
2023-05-02 18:47:27 +0000

    vidispark

--- Documents/vidispark/src/components/Nft.js
+++ Documents/vidispark/src/components/Nft.js
@@ -6,12 +6,6 @@ const Nft = ({ data }) => {
     <div className="featured-item">
       <div className="featured-item-wrapper">
         <div className="featured-item-content">
-          <div className="fav-counter">
-            <svg className="crumina-icon">
-              <use xlinkHref="#heart-icon" />
-            </svg>{" "}
-            {/* <span className="count">{data.react}</span> */}
-          </div>
           <div className="featured-item-image">
             {" "}
             <Link href="/05-product-page">
