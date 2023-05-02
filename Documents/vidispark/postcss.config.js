71b764109108a99ffc593583214b8a089712cb6a
mendsalbert <mendsalbert@gmail.com>
2023-05-02 09:12:18 +0000

    vidispark

--- Documents/vidispark/postcss.config.js
+++ Documents/vidispark/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  plugins: [
-    require("autoprefixer"),
-    require("postcss-import"),
-    require("tailwindcss"),
-  ],
+  plugins: {
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
