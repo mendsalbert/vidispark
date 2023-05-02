4bfa8af012ceffec31754d38c778affff29129ab
mendsalbert <mendsalbert@gmail.com>
2023-05-02 08:20:49 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -16,6 +16,19 @@ const ProfileUploadAndPreview = () => {
             </button>
           </div>
         </div>
+
+        <div className="artwork-upload">
+          <div className="label">Upload File</div>
+          <div className="upload-box">
+            <svg className="crumina-icon">
+              <use xlinkHref="#upload-icon" />
+            </svg>
+            <div className="upload-notice"> Max 120MB, PNG, JPEG, MP3, MP4</div>
+            <button className="btn btn-normal btn-dark browse-btn">
+              Browse File
+            </button>
+          </div>
+        </div>
         <div className="artwork-preview">
           <div className="label">File Preview </div>
           <div className="preview-box" />
