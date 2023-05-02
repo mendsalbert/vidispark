fb0dde5985ed04f285048a514a0b6d68daacd1ff
mendsalbert <mendsalbert@gmail.com>
2023-05-02 11:06:39 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -23,9 +23,9 @@ const ProfileUploadAndPreview = () => {
             <div className="upload-notice"> Start a live stream</div>
             <button className="tw-bg-red-600 tw-rounded-full tw-px-3 tw-py-2">
               <span>Go Live</span>
-              <span class="relative flex h-3 w-3">
-                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+              <span class="tw-relative tw-flex tw-h-3 tw-w-3">
+                <span class="tw-animate-ping tw-absolute tw-inline-flex tw-h-full tw-w-full tw-rounded-full tw-bg-sky-400 tw-opacity-75"></span>
+                <span class="tw-relative tw-inline-flex tw-rounded-full tw-h-3 tw-w-3 tw-bg-sky-500"></span>
               </span>
             </button>
           </div>
