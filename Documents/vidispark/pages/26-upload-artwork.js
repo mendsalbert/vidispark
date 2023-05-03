b1eae01804a889da6d4744807f5d96bee85cc294
mendsalbert <mendsalbert@gmail.com>
2023-05-03 16:48:44 +0000

    vidispark

--- Documents/vidispark/pages/26-upload-artwork.js
+++ Documents/vidispark/pages/26-upload-artwork.js
@@ -33,27 +33,130 @@
 // };
 // export default UploadArtWork;
 
+//GOLD
+// import React, { useState } from "react";
+// import axios from "axios";
+// function VideoUploader() {
+//   const [uploadUrl, setUploadUrl] = useState("");
+//   const [videoUrl, setVideoUrl] = useState("");
+
+//   const handleFileChange = async (event) => {
+//     const file = event.target.files[0];
+//     const uploadResponse = await fetch("https://api.thetavideoapi.com/upload", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "x-tva-sa-id": "srvacc_fk130i83e047t4kg5w4edswj7",
+//         "x-tva-sa-secret": "6hvuhqk3499qu9gbb8w34gft6q6q8re5",
+//       },
+//       body: JSON.stringify({}),
+//     });
+//     const uploadResponseJson = await uploadResponse.json();
+//     const { presigned_url } = uploadResponseJson.body.uploads[0];
+//     setUploadUrl(presigned_url);
+//     await fetch(presigned_url, {
+//       method: "PUT",
+//       headers: { "Content-Type": "application/octet-stream" },
+//       body: file,
+//     });
+//     const videoResponse = await fetch("https://api.thetavideoapi.com/video", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "x-tva-sa-id": "srvacc_fk130i83e047t4kg5w4edswj7",
+//         "x-tva-sa-secret": "6hvuhqk3499qu9gbb8w34gft6q6q8re5",
+//       },
+//       body: JSON.stringify({
+//         source_upload_id: uploadResponseJson.body.uploads[0].id,
+//         playback_policy: "public",
+//         nft_collection: "0x5d0004fe2e0ec6d002678c7fa01026cabde9e793",
+//       }),
+//     });
+//     const videoResponseJson = await videoResponse.json();
+//     console.log(videoResponseJson.body.videos);
+//     let finished = false;
+
+//     while (!finished) {
+//       const { data } = await axios.get(
+//         `https://api.thetavideoapi.com/video/${videoResponseJson.body.videos[0].id}`,
+//         {
+//           headers: {
+//             "x-tva-sa-id": "srvacc_fk130i83e047t4kg5w4edswj7",
+//             "x-tva-sa-secret": "6hvuhqk3499qu9gbb8w34gft6q6q8re5",
+//           },
+//         }
+//       );
+//       console.log("data", data);
+//       if (
+//         data?.body?.videos?.[0]?.state === "success" &&
+//         data?.body?.videos?.[0]?.sub_state === "none"
+//       ) {
+//         finished = true;
+//         setVideoUrl(data.body.videos[0].player_uri);
+//       } else {
+//         await new Promise((resolve) => setTimeout(resolve, 1000)); // wait one second before checking again
+//       }
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <input type="file" onChange={handleFileChange} />
+//       {videoUrl && <video controls src={videoUrl} />}
+//       {/* <iframe
+//         src="https://player.thetavideoapi.com/video/video_5v9q854twpr893bz4nktx22xkh"
+//         border="0"
+//         width="100%"
+//         height="100%"
+//         allowfullscreen
+//       /> */}
+//     </div>
+//   );
+// }
+
+// export default VideoUploader;
+
 import React, { useState } from "react";
 import axios from "axios";
+
 function VideoUploader() {
   const [uploadUrl, setUploadUrl] = useState("");
   const [videoUrl, setVideoUrl] = useState("");
+  const [streamUrl, setStreamUrl] = useState("");
 
+  // create a new live stream
+  const createStream = async () => {
+    try {
+      const response = await axios.post(
+        "https://api.thetavideoapi.com/stream",
+        {
+          name: "My New Livestream",
+          resolutions: ["160p", "240p", "360p", "720p", "source"],
+          source_resolution: "720p",
+          fps: 60,
+        },
+        {
+          headers: {
+            "x-tva-sa-id": "srvacc_fk130i83e047t4kg5w4edswj7",
+            "x-tva-sa-secret": "6hvuhqk3499qu9gbb8w34gft6q6q8re5",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const streamUrl = `${response.data.body.stream_server}/${response.data.body.stream_key}`;
+      setStreamUrl(streamUrl);
+    } catch (error) {
+      console.error("Failed to create live stream", error);
+    }
+  };
+
+  // handle the file upload
   const handleFileChange = async (event) => {
+    await createStream(); // create a new live stream before uploading the video
+
     const file = event.target.files[0];
-    const uploadResponse = await fetch("https://api.thetavideoapi.com/upload", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-tva-sa-id": "srvacc_fk130i83e047t4kg5w4edswj7",
-        "x-tva-sa-secret": "6hvuhqk3499qu9gbb8w34gft6q6q8re5",
-      },
-      body: JSON.stringify({}),
-    });
-    const uploadResponseJson = await uploadResponse.json();
-    const { presigned_url } = uploadResponseJson.body.uploads[0];
-    setUploadUrl(presigned_url);
-    await fetch(presigned_url, {
+    const uploadResponse = await fetch(uploadUrl, {
       method: "PUT",
       headers: { "Content-Type": "application/octet-stream" },
       body: file,
@@ -66,7 +169,7 @@ function VideoUploader() {
         "x-tva-sa-secret": "6hvuhqk3499qu9gbb8w34gft6q6q8re5",
       },
       body: JSON.stringify({
-        source_upload_id: uploadResponseJson.body.uploads[0].id,
+        source_upload_id: uploadResponse.body.uploads[0].id,
         playback_policy: "public",
         nft_collection: "0x5d0004fe2e0ec6d002678c7fa01026cabde9e793",
       }),
@@ -101,14 +204,25 @@ function VideoUploader() {
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
-      {videoUrl && <video controls src={videoUrl} />}
-      {/* <iframe
-        src="https://player.thetavideoapi.com/video/video_5v9q854twpr893bz4nktx22xkh"
-        border="0"
-        width="100%"
-        height="100%"
-        allowfullscreen
-      /> */}
+      {streamUrl && (
+        <div>
+          <div>Live stream URL: {streamUrl}</div>
+          <iframe
+            src={streamUrl}
+            width="640"
+            height="360"
+            frameborder="0"
+            scrolling="no"
+            allowfullscreen
+          ></iframe>
+        </div>
+      )}
+      {videoUrl && (
+        <div>
+          <div>Uploaded video URL: {videoUrl}</div>
+          <video controls src={videoUrl} />
+        </div>
+      )}
     </div>
   );
 }
