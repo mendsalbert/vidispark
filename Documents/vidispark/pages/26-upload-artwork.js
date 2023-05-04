af8178d81bf5deaee615b641cd4bcbc27e5013a4
mendsalbert <mendsalbert@gmail.com>
2023-05-04 07:30:09 +0000

    vidispark

--- Documents/vidispark/pages/26-upload-artwork.js
+++ Documents/vidispark/pages/26-upload-artwork.js
@@ -1,37 +1,37 @@
-// import React from "react";
-// import ProfileInfoSidebar from "../src/components/Profile Page/Profile Info/ProfileInfoSidebar";
-// import ProfilePurchase from "../src/components/Profile Page/Profile Upload/ProfilePurchase";
-// import ProfileUploadAndPreview from "../src/components/Profile Page/Profile Upload/ProfileUploadAndPreview";
-// import Layouts from "../src/layouts/Layouts";
+import React from "react";
+import ProfileInfoSidebar from "../src/components/Profile Page/Profile Info/ProfileInfoSidebar";
+import ProfilePurchase from "../src/components/Profile Page/Profile Upload/ProfilePurchase";
+import ProfileUploadAndPreview from "../src/components/Profile Page/Profile Upload/ProfileUploadAndPreview";
+import Layouts from "../src/layouts/Layouts";
 
-// const UploadArtWork = () => {
-//   return (
-//     <Layouts>
-//       <div className="primary-content-area container content-padding grid-left-sidebar">
-//         {/* / Profile Info Sidebar */}
-//         <ProfileInfoSidebar />
-//         {/* / Profile Info Sidebar */}
-//         <div className="main-content-area">
-//           <div className="page-title">
-//             <h2>
-//               <span className="gradient-text">Upload</span> Artwork
-//             </h2>
-//           </div>
-//           <div className="user-db-content-area">
-//             {/* / Profile Upload & Preview */}
-//             <ProfileUploadAndPreview />
-//             {/* / Profile Upload & Preview */}
+const UploadArtWork = () => {
+  return (
+    <Layouts>
+      <div className="primary-content-area container content-padding grid-left-sidebar">
+        {/* / Profile Info Sidebar */}
+        <ProfileInfoSidebar />
+        {/* / Profile Info Sidebar */}
+        <div className="main-content-area">
+          <div className="page-title">
+            <h2>
+              <span className="gradient-text">Upload</span> Artwork
+            </h2>
+          </div>
+          <div className="user-db-content-area">
+            {/* / Profile Upload & Preview */}
+            <ProfileUploadAndPreview />
+            {/* / Profile Upload & Preview */}
 
-//             {/* / Profile Purchase */}
-//             <ProfilePurchase />
-//             {/* / Profile Purchase */}
-//           </div>
-//         </div>
-//       </div>
-//     </Layouts>
-//   );
-// };
-// export default UploadArtWork;
+            {/* / Profile Purchase */}
+            <ProfilePurchase />
+            {/* / Profile Purchase */}
+          </div>
+        </div>
+      </div>
+    </Layouts>
+  );
+};
+export default UploadArtWork;
 
 //GOLD
 // import React, { useState } from "react";
@@ -115,104 +115,104 @@
 // }
 
 // export default VideoUploader;
-import React, { useState, useRef } from "react";
-import axios from "axios";
+// import React, { useState, useRef } from "react";
+// import axios from "axios";
 
-function ThetaLiveStreaming() {
-  const [streamUrl, setStreamUrl] = useState("");
-  const [isStreaming, setIsStreaming] = useState(false);
-  const videoRef = useRef(null);
-  const streamRef = useRef(null);
+// function ThetaLiveStreaming() {
+//   const [streamUrl, setStreamUrl] = useState("");
+//   const [isStreaming, setIsStreaming] = useState(false);
+//   const videoRef = useRef(null);
+//   const streamRef = useRef(null);
 
-  const startStream = async () => {
-    setIsStreaming(true);
-    const streamData = new FormData();
-    streamData.append("name", "My Live Stream");
-    streamData.append("resolutions", [
-      "160p",
-      "240p",
-      "360p",
-      "720p",
-      "source",
-    ]);
-    streamData.append("source_resolution", "720p");
-    streamData.append("fps", "60");
+//   const startStream = async () => {
+//     setIsStreaming(true);
+//     const streamData = new FormData();
+//     streamData.append("name", "My Live Stream");
+//     streamData.append("resolutions", [
+//       "160p",
+//       "240p",
+//       "360p",
+//       "720p",
+//       "source",
+//     ]);
+//     streamData.append("source_resolution", "720p");
+//     streamData.append("fps", "60");
 
-    const response = await axios.post(
-      "https://api.thetavideoapi.com/stream",
-      streamData,
-      {
-        headers: {
-          "x-tva-sa-id": "srvacc_fk130i83e047t4kg5w4edswj7",
-          "x-tva-sa-secret": "6hvuhqk3499qu9gbb8w34gft6q6q8re5",
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
+//     const response = await axios.post(
+//       "https://api.thetavideoapi.com/stream",
+//       streamData,
+//       {
+//         headers: {
+//           "x-tva-sa-id": "srvacc_fk130i83e047t4kg5w4edswj7",
+//           "x-tva-sa-secret": "6hvuhqk3499qu9gbb8w34gft6q6q8re5",
+//           "Content-Type": "multipart/form-data",
+//         },
+//       }
+//     );
 
-    const streamKey = response.data.body.stream_key;
-    const streamServer = response.data.body.stream_server;
+//     const streamKey = response.data.body.stream_key;
+//     const streamServer = response.data.body.stream_server;
 
-    setStreamUrl(`${streamServer}/${streamKey}`);
+//     setStreamUrl(`${streamServer}/${streamKey}`);
 
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+//     const stream = await navigator.mediaDevices.getUserMedia({
+//       audio: true,
+//       video: true,
+//     });
 
-    streamRef.current = stream;
-    videoRef.current.srcObject = stream;
-    videoRef.current.play();
+//     streamRef.current = stream;
+//     videoRef.current.srcObject = stream;
+//     videoRef.current.play();
 
-    const publishStreamUrl = `rtmp://ingestor.thetavideoapi.com/app/${streamKey}`;
+//     const publishStreamUrl = `rtmp://ingestor.thetavideoapi.com/app/${streamKey}`;
 
-    const ws = new WebSocket("ws://ingestor.thetavideoapi.com/app");
+//     const ws = new WebSocket("ws://ingestor.thetavideoapi.com/app");
 
-    ws.addEventListener("open", () => {
-      console.log("Connection established");
-      ws.send(`{"command":"publish","streamName":"${streamKey}"}`);
-    });
+//     ws.addEventListener("open", () => {
+//       console.log("Connection established");
+//       ws.send(`{"command":"publish","streamName":"${streamKey}"}`);
+//     });
 
-    ws.addEventListener("message", (event) => {
-      console.log(`[message] Data received from server: ${event.data}`);
-      const message = JSON.parse(event.data);
-      if (message.status == "publishing") {
-        console.log("Stream is publishing");
-      }
-    });
+//     ws.addEventListener("message", (event) => {
+//       console.log(`[message] Data received from server: ${event.data}`);
+//       const message = JSON.parse(event.data);
+//       if (message.status == "publishing") {
+//         console.log("Stream is publishing");
+//       }
+//     });
 
-    const wsHeartbeatInterval = setInterval(() => {
-      ws.send('{"command":"ping"}');
-    }, 5000);
+//     const wsHeartbeatInterval = setInterval(() => {
+//       ws.send('{"command":"ping"}');
+//     }, 5000);
 
-    window.addEventListener("beforeunload", () => {
-      clearInterval(wsHeartbeatInterval);
-      ws.close();
-      streamRef.current.getTracks().forEach((track) => track.stop());
-    });
-  };
+//     window.addEventListener("beforeunload", () => {
+//       clearInterval(wsHeartbeatInterval);
+//       ws.close();
+//       streamRef.current.getTracks().forEach((track) => track.stop());
+//     });
+//   };
 
-  const stopStream = () => {
-    setIsStreaming(false);
-    streamRef.current.getTracks().forEach((track) => track.stop());
-  };
+//   const stopStream = () => {
+//     setIsStreaming(false);
+//     streamRef.current.getTracks().forEach((track) => track.stop());
+//   };
 
-  return (
-    <div>
-      {!isStreaming && <button onClick={startStream}>Start Live Stream</button>}
-      {isStreaming && <button onClick={stopStream}>Stop Live Stream</button>}
-      {streamUrl && (
-        <div>
-          <div>
-            <a href={streamUrl} target="_blank">
-              Playback Live Stream
-            </a>
-          </div>
-          <video ref={videoRef} width="640" height="480" controls></video>
-        </div>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {!isStreaming && <button onClick={startStream}>Start Live Stream</button>}
+//       {isStreaming && <button onClick={stopStream}>Stop Live Stream</button>}
+//       {streamUrl && (
+//         <div>
+//           <div>
+//             <a href={streamUrl} target="_blank">
+//               Playback Live Stream
+//             </a>
+//           </div>
+//           <video ref={videoRef} width="640" height="480" controls></video>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
-export default ThetaLiveStreaming;
+// export default ThetaLiveStreaming;
