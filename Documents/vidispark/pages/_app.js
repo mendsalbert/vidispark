8b20b988b6ad9f5dc5ff403bd85838b86b9671c8
mendsalbert <mendsalbert@gmail.com>
2023-05-04 08:42:31 +0000

    vidispark

--- Documents/vidispark/pages/_app.js
+++ Documents/vidispark/pages/_app.js
@@ -4,6 +4,64 @@ import { Provider } from "react-redux";
 import store from "../src/redux/store";
 import { countdown } from "../src/utils";
 import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+// import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+
+const xdcApothem = {
+  id: 51,
+  name: "Apothem-Network (TestNet)",
+  network: "XDC Apothem Network (TestNet)",
+  nativeCurrency: {
+    decimals: 18,
+    name: "XDC-Network",
+    symbol: "XDC",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://erpc.apothem.network"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Apothem Explorer",
+      url: "https://explorer.apothem.network/",
+    },
+  },
+  testnet: true,
+};
+const { provider, chains } = configureChains(
+  [xdcApothem],
+  [
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Sustain",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+const myTheme = merge(midnightTheme(), {
+  colors: {
+    accentColor: "#18181b",
+    accentColorForeground: "#fff",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -22,7 +80,11 @@ function MyApp({ Component, pageProps }) {
 
         <title>NFT</title>
       </Head>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={myTheme}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </Provider>
   );
 }
