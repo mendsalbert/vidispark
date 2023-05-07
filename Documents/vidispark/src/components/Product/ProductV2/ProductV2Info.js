33686524e341d6e457d2b83555242e98f1b332d5
mendsalbert <mendsalbert@gmail.com>
2023-05-07 20:24:26 +0000

    vidispark

--- Documents/vidispark/src/components/Product/ProductV2/ProductV2Info.js
+++ Documents/vidispark/src/components/Product/ProductV2/ProductV2Info.js
@@ -21,28 +21,7 @@ const ProductV2Info = () => {
           <div className="avatar-meta">@DexterStark</div>
         </div>
       </div>
-      <div className="product-meta-section">
-        <div className="product-meta-item">
-          <div className="label">Current bid</div>
-          <div className="value">2.31 ETH</div>
-          <div className="caption">125.230 U$D</div>
-        </div>
-        <div className="product-meta-item">
-          <div className="label">Edition</div>
-          <div className="value">01/40</div>
-          <div className="caption">Tokens</div>
-        </div>
-        <div className="product-meta-item">
-          <div className="label">AUCTION ENDING IN</div>
-          <div className="countdown">
-            <div
-              className="js-countdown"
-              data-timer={104400}
-              data-labels="Days , Hours , Mins , Secs"
-            />
-          </div>
-        </div>
-      </div>
+
       <div className="place-bid">
         <button className="btn btn-wide btn-dark">Place a Bid!</button>
       </div>
