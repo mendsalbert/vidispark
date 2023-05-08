27126aca38e280ac63cae68ae6f1021136f7596f
mendsalbert <mendsalbert@gmail.com>
2023-05-08 19:11:06 +0000

    vidispark

--- Documents/vidispark/pages/26-upload-artwork.js
+++ Documents/vidispark/pages/26-upload-artwork.js
@@ -223,3 +223,29 @@
 //     </div>
 //   );
 // }
+// Import ComposeDB client
+
+import { ComposeClient } from "@composedb/client";
+
+// Import your compiled composite
+
+import { definition } from "../definition";
+
+// Create an instance of ComposeClient
+// Pass the URL of your Ceramic server
+// Pass reference to your compiled composite
+
+const compose = new ComposeClient({
+  ceramic: "http://localhost:7007",
+  definition,
+});
+
+const get = async () => {
+  await compose.executeQuery(`
+  query {
+    viewer {
+      id
+    }
+  }
+`);
+};
