f3617ca1ad728276af8d55710397fbd38be0be63
mendsalbert <mendsalbert@gmail.com>
2023-05-08 19:32:33 +0000

    vidispark

--- Documents/vidispark/pages/_app.js
+++ Documents/vidispark/pages/_app.js
@@ -24,7 +24,6 @@ import {
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 // import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
-import definition from "../definition";
 
 const xdcApothem = {
   id: 51,
