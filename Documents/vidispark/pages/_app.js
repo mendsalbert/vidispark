7feee6f78df84e09546a4a5643211f92051235eb
mendsalbert <mendsalbert@gmail.com>
2023-05-08 20:29:42 +0000

    vidispark

--- Documents/vidispark/pages/_app.js
+++ Documents/vidispark/pages/_app.js
@@ -17,7 +17,6 @@ import {
 import { ComposeClient } from "@composedb/client";
 import { Web3Provider } from "@ethersproject/providers";
 import Web3Modal from "web3modal";
-import { ComposeClient } from "@composedb/client";
 import { DIDSession } from "did-session";
 import { EthereumWebAuth, getAccountId } from "@didtools/pkh-ethereum";
 import {
