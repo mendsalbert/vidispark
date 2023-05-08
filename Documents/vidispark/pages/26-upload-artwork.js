a8ea386742b2b739ba33d24b5678a89bbaa99243
mendsalbert <mendsalbert@gmail.com>
2023-05-08 16:27:18 +0000

    vidispark

--- Documents/vidispark/pages/26-upload-artwork.js
+++ Documents/vidispark/pages/26-upload-artwork.js
@@ -303,6 +303,22 @@ export default function App() {
     return user;
   }
 
+  const makeFileObjects = () => {
+    // You can create File objects from a Blob of binary data
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+    // Here we're just storing a JSON object, but you can store images,
+    // audio, or whatever you want!
+    const obj = { hello: "world" };
+    const blob = new Blob([JSON.stringify(obj)], { type: "application/json" });
+
+    const files = [
+      new File(["contents-of-file-1"], "plain-utf8.txt"),
+      new File([blob], "hello.json"),
+    ];
+    console.log(files);
+    return files;
+  };
+
   return (
     <div>
       <button
