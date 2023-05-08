f4f35989a72861274f0e8ea3ba7cd290309789ba
mendsalbert <mendsalbert@gmail.com>
2023-05-08 19:14:38 +0000

    vidispark

--- Documents/vidispark/pages/26-upload-artwork.js
+++ Documents/vidispark/pages/26-upload-artwork.js
@@ -243,14 +243,14 @@ const compose = new ComposeClient({
 
 const get = async () => {
   let res = await compose.executeQuery(`
-  {
+  
     viewer {
       profile {
         name
         bio
       }
     }
-  }
+
   `);
 
   console.log("res", res);
