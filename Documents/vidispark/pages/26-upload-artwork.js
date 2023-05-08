834dea1ae5624fd56c8972d775807cd8ad8d82a8
mendsalbert <mendsalbert@gmail.com>
2023-05-08 16:10:18 +0000

    vidispark

--- Documents/vidispark/pages/26-upload-artwork.js
+++ Documents/vidispark/pages/26-upload-artwork.js
@@ -224,299 +224,78 @@
 //   );
 // }
 
-// export default ThetaLiveStreaming;
-
-import { useRef, useState, useEffect } from "react";
-import lf from "localforage";
-import { isNil, map } from "ramda";
-import SDK from "weavedb-sdk";
-import { Buffer } from "buffer";
-import { ethers } from "ethers";
-
 export default function App() {
-  // let db = null;
-  const contractTxId = "0P-YuG46ghkoxUTiZ_rkRsnqxxlTLVpzYVLd5FXwA80";
+  // Require the web3storage module
+  const { Web3Storage } = require("web3.storage");
 
-  const [user, setUser] = useState(null);
-  const [tasks, setTasks] = useState([]);
-  const [tab, setTab] = useState("All");
-  const [db, setdb] = useState("");
-  const [initDB, setInitDB] = useState(false);
-  let task = useRef();
-  const tabs = isNil(user) ? ["All"] : ["All", "Yours"];
-  console.log(tasks);
-  const setupWeaveDB = async () => {
-    window.Buffer = Buffer;
-    let db = new SDK({
-      contractTxId,
-    });
-    await db.initializeWithoutWallet();
-    setdb(db);
-    setInitDB(true);
-  };
+  // Create a new instance of Web3Storage
+  const client = new Web3Storage({ token: "your-api-key" });
 
-  // console.log(db);
+  // Define a function for uploading user data to web3storage
+  async function uploadUserData(user, data) {
+    // Convert the data to a Uint8Array
+    const uint8Data = new TextEncoder().encode(JSON.stringify(data));
 
-  const getTasks = async () => {
-    setTasks(await db.cget("tasks", ["date", "desc"]));
-  };
+    // Upload the data to web3storage and get the resulting CID
+    const { cid } = await client.put(uint8Data);
 
-  const getMyTasks = async () => {
-    setTasks(
-      await db.cget(
-        "tasks",
-        ["user_address", "==", user.wallet.toLowerCase()],
-        ["date", "desc"]
-      )
-    );
-  };
+    // Save the CID to the user's profile
+    user.web3storageCid = cid.toString();
+  }
 
-  const addTask = async (task) => {
-    await db.add(
-      {
-        task,
-        date: db.ts(),
-        // user_address: db.signer(),
-        done: false,
-      },
-      "tasks",
-      user
-    );
-    await getTasks();
-  };
+  // Define a function for downloading user data from web3storage
+  async function downloadUserData(user) {
+    // Retrieve the data from web3storage using the CID
+    const res = await fetch(`https://dweb.link/ipfs/${user.web3storageCid}`);
+    const data = await res.json();
 
-  const completeTask = async (id) => {
-    await db.update(
-      {
-        done: true,
-      },
-      "tasks",
-      id,
-      user
-    );
-    await getTasks();
-  };
+    // Return the data
+    return data;
+  }
 
-  const deleteTask = async (id) => {
-    await db.delete("tasks", id, user);
-    await getTasks();
-  };
+  // Define a function for authenticating a user
+  async function authenticateUser(username, password) {
+    // Retrieve the user's data from web3storage
+    const user = await downloadUserData(username);
 
-  const login = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    await provider.send("eth_requestAccounts", []);
-    const wallet_address = await provider.getSigner().getAddress();
-    let identity = await lf.getItem(
-      `temp_address:${contractTxId}:${wallet_address}`
-    );
-    let tx;
-    let err;
-    if (isNil(identity)) {
-      ({ tx, identity, err } = await db.createTempAddress(wallet_address));
-      const linked = await db.getAddressLink(identity.address);
-      if (isNil(linked)) {
-        alert("something went wrong");
-        return;
-      }
+    // Check if the password is correct
+    if (user.password === password) {
+      // Return the user object if the password is correct
+      return user;
     } else {
-      await lf.setItem("temp_address:current", wallet_address);
-      setUser({
-        wallet: wallet_address,
-        privateKey: identity.privateKey,
-      });
-      return;
+      // Throw an error if the password is incorrect
+      throw new Error("Incorrect password");
     }
-    if (!isNil(tx) && isNil(tx.err)) {
-      identity.tx = tx;
-      identity.linked_address = wallet_address;
-      await lf.setItem("temp_address:current", wallet_address);
-      await lf.setItem(
-        `temp_address:${contractTxId}:${wallet_address}`,
-        identity
-      );
-      setUser({
-        wallet: wallet_address,
-        privateKey: identity.privateKey,
-      });
+  }
+
+  // Define a function for creating a new user
+  async function createUser(username, password, profileImage) {
+    // Check if the username is available
+    const existingUser = await downloadUserData(username);
+    if (existingUser) {
+      throw new Error("Username already taken");
     }
-  };
 
-  const logout = async () => {
-    if (confirm("Would you like to sign out?")) {
-      await lf.removeItem("temp_address:current");
-      setUser(null, "temp_current");
-    }
-  };
+    // Upload the user's profile image to IPFS and get the resulting CID
+    const { cid } = await client.put(profileImage);
 
-  const checkUser = async () => {
-    const wallet_address = await lf.getItem(`temp_address:current`);
-    if (!isNil(wallet_address)) {
-      const identity = await lf.getItem(
-        `temp_address:${contractTxId}:${wallet_address}`
-      );
-      if (!isNil(identity))
-        setUser({
-          wallet: wallet_address,
-          privateKey: identity.privateKey,
-        });
-    }
-  };
-
-  useEffect(() => {
-    checkUser();
-    setupWeaveDB();
-  }, []);
-
-  useEffect(() => {
-    if (initDB) {
-      if (tab === "All") {
-        getTasks();
-      } else {
-        getMyTasks();
-      }
-    }
-  }, [tab, initDB]);
-
-  const NavBar = () => (
-    <div className="fixed w-full top-0 left-0 py-3 px-4 flex justify-end bg-white shadow">
-      <div className="flex-1"></div>
-      <div
-        className="px-6 py-2 bg-black text-white cursor-pointer rounded-md hover:opacity-75"
-        style={{ borderRadius: "5px" }}
-        onClick={() => (isNil(user) ? login() : logout())}
-      >
-        {!isNil(user) ? (
-          <span>{user.wallet.slice(0, 7)}</span>
-        ) : (
-          <span>Connect Wallet</span>
-        )}
-      </div>
-    </div>
-  );
-
-  const Tabs = () => (
-    <div className="mb-4 flex justify-center">
-      {" "}
-      {map((v) => (
-        <div
-          key={v}
-          className={`mx-2 cursor-pointer ${
-            tab === v && "text-red-500 underline"
-          }`}
-          onClick={() => setTab(v)}
-        >
-          {" "}
-          {v}{" "}
-        </div>
-      ))(tabs)}{" "}
-    </div>
-  );
-
-  const Tasks = () =>
-    map((v) => (
-      <div
-        key={v.id}
-        className="flex my-1 p-3 border border-gray-300 rounded-md"
-        style={{ borderRadius: "5px" }}
-      >
-        {" "}
-        <div
-          className="w-30px text-center cursor-pointer hover:opacity-75"
-          style={{ cursor: "pointer" }}
-        >
-          {" "}
-          {v.data.done ? (
-            "✅"
-          ) : v.data.user_address !== user?.wallet.toLowerCase() ? null : (
-            <span onClick={() => completeTask(v.id)}>⬜</span>
-          )}{" "}
-        </div>{" "}
-        <div className="px-3 flex-1 ml-10" style={{ marginLeft: "10px" }}>
-          {" "}
-          {v.data.task}{" "}
-        </div>{" "}
-        <div className="w-100px text-center ml-10">
-          {" "}
-          {v.data.user_address.slice(0, 7)}{" "}
-        </div>{" "}
-        <div
-          className="w-50px text-center cursor-pointer hover:opacity-75"
-          style={{ cursor: "pointer" }}
-        >
-          {" "}
-          {v.data.user_address === user?.wallet.toLowerCase() ? (
-            <span
-              style={{ marginLeft: "10px" }}
-              onClick={() => deleteTask(v.id)}
-            >
-              {" "}
-              ❌{" "}
-            </span>
-          ) : null}{" "}
-        </div>{" "}
-      </div>
-    ))(tasks);
-
-  const NewTask = () => {
-    const [newTask, setNewTask] = useState("");
-
-    const handleAddBtnClick = async () => {
-      if (!/^\s*$/.test(newTask)) {
-        await addTask(newTask);
-        setNewTask("");
-      }
+    // Create a new user object with the provided data
+    const user = {
+      username,
+      password,
+      profileImageCid: cid.toString(),
+      web3storageCid: null,
+      followers: [],
+      following: [],
+      tokenBalance: 0,
     };
 
-    return (
-      <div className="mb-4 flex">
-        <input
-          type="text"
-          placeholder="Enter New Task"
-          className="rounded-l-md border border-gray-400 py-2 px-4 outline-none focus:border-blue-500 flex-1"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-        />
-        <div
-          className="px-6 py-2 bg-black text-white cursor-pointer rounded-r-md hover:opacity-75"
-          style={{ borderRadius: "5px" }}
-          onClick={handleAddBtnClick}
-        >
-          add
-        </div>
-      </div>
-    );
-  };
+    // Upload the user object to web3storage
+    await uploadUserData(user);
 
-  const Transactions = () => {
-    return (
-      <div className="py-4 flex justify-center">
-        {" "}
-        <a
-          href={`https://sonar.warp.cc/?#/app/contract/${contractTxId}`}
-          target="_blank"
-          rel="noreferrer"
-          className="underline"
-        >
-          {" "}
-          view transactions{" "}
-        </a>{" "}
-      </div>
-    );
-  };
+    // Return the new user object
+    return user;
+  }
 
-  return (
-    <div>
-      {" "}
-      <NavBar />{" "}
-      <div className="mt-60px flex justify-center p-3">
-        {" "}
-        <div className="w-full max-w-600px">
-          {" "}
-          <Tabs /> {isNil(user) ? <NewTask /> : null} <Tasks />{" "}
-          {/* <NewTask /> */}
-        </div>{" "}
-      </div>{" "}
-      <Transactions />{" "}
-    </div>
-  );
+  return <div></div>;
 }
