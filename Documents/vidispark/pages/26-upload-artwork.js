47485179415a36ed53fdc379205e5cc9a371a76a
mendsalbert <mendsalbert@gmail.com>
2023-05-09 06:39:42 +0000

    vidispark

--- Documents/vidispark/pages/26-upload-artwork.js
+++ Documents/vidispark/pages/26-upload-artwork.js
@@ -230,25 +230,9 @@ import { gql, useMutation, useQuery } from "@apollo/client";
 
 const LIST_SIMPLE_PROFILES = gql`
   query {
-    {
-        profile: node(id: "k2t6wzhkhabz3toab3koplwrdbtpvf1qdp5t4jz2s5bfyoejh6330cd7hxteq0") {
-          ... on Profile {
-            name
-            bio
-            author {
-              articleList(last: 10) {
-                articles: edges {
-                  article: node {
-                    id
-                    date
-                    title
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+    viewer {
+      id
+    }
   }
 `;
 const ADD_SIMPLE_PROFILE = gql`
