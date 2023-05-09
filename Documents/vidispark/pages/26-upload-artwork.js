aad54e50dd718acc3a2687a93ec3a1e1bae69e1e
mendsalbert <mendsalbert@gmail.com>
2023-05-09 08:53:41 +0000

    vidispark

--- Documents/vidispark/pages/26-upload-artwork.js
+++ Documents/vidispark/pages/26-upload-artwork.js
@@ -242,11 +242,12 @@ const LIST_SIMPLE_PROFILES = gql`
 const ADD_SIMPLE_PROFILE = gql`
   mutation {
     createProfile(
-      input: { content: { name: "Admin2", bio: "The creator of this blog." } }
+      input: { content: { name: "Admin", bio: "The creator of this blog." } }
     ) {
       profile: document {
         name
         bio
+        id
       }
     }
   }
