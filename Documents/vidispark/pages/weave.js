9ef91e1ad735bbe14604a2a4431241f2b18481fc
mendsalbert <mendsalbert@gmail.com>
2023-05-10 20:05:26 +0000

    vidispark

--- Documents/vidispark/pages/weave.js
+++ Documents/vidispark/pages/weave.js
@@ -81,31 +81,24 @@ export default function App() {
 
   const login = async () => {
     try {
-      const providerOptions = {};
-      const web3Modal = new Web3Modal({
-        cacheProvider: true,
-        providerOptions,
-      });
-
       const provider = await web3Modal.connect();
       const web3Provider = new Web3Provider(provider);
       const accounts = await web3Provider.listAccounts();
       const wallet_address = accounts[0];
-      let identity = JSON.parse(
-        localStorage.getItem(`temp_address:${contractTxId}:${wallet_address}`)
+      let identity = await lf.getItem(
+        `temp_address:${contractTxId}:${wallet_address}`
       );
       let tx;
       let err;
       if (isNil(identity)) {
         ({ tx, identity, err } = await db.createTempAddress(wallet_address));
         const linked = await db.getAddressLink(identity.address);
-        console.log(linked);
         if (isNil(linked)) {
           alert("something went wrong");
           return;
         }
       } else {
-        localStorage.setItem("temp_address:current", wallet_address);
+        await lf.setItem("temp_address:current", wallet_address);
         setUser({
           wallet: wallet_address,
           privateKey: identity.privateKey,
@@ -115,10 +108,10 @@ export default function App() {
       if (!isNil(tx) && isNil(tx.err)) {
         identity.tx = tx;
         identity.linked_address = wallet_address;
-        localStorage.setItem("temp_address:current", wallet_address);
-        localStorage.setItem(
+        await lf.setItem("temp_address:current", wallet_address);
+        await lf.setItem(
           `temp_address:${contractTxId}:${wallet_address}`,
-          JSON.stringify(identity)
+          identity
         );
         setUser({
           wallet: wallet_address,
