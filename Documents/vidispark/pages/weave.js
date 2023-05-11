b479365f6af5b3e907da67b6890e62384ae59c9c
mendsalbert <mendsalbert@gmail.com>
2023-05-11 04:48:00 +0000

    vidispark

--- Documents/vidispark/pages/weave.js
+++ Documents/vidispark/pages/weave.js
@@ -10,25 +10,17 @@ import Web3Modal from "web3modal";
 import { Web3Provider } from "@ethersproject/providers";
 
 const contractTxId = "tqRK32rdpnBNQ3rGTwfGZVAYBSgKCVkfYh9JTmd9v_c";
-
+import { useAuth } from "../src/auth";
 export default function App() {
   const [tasks, setTasks] = useState([]);
   const [tab, setTab] = useState("All");
-  const [user, setUser] = useState(null);
-  const [initDB, setInitDB] = useState(false);
-  const [db, setdb] = useState("");
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  const [initDB, setInitDB] = useState(localStorage.getItem("initDB"));
+  const [db, setdb] = useState(JSON.parse(localStorage.getItem("db")));
   let task = useRef();
   const tabs = isNil(user) ? ["All"] : ["All", "Yours"];
 
-  //   const setupWeaveDB = async () => {
-  //     window.Buffer = Buffer;
-  //     let db = new SDK({
-  //       contractTxId,
-  //     });
-  //     setdb(db);
-  //     await db.initializeWithoutWallet();
-  //     setInitDB(true);
-  //   };
+  const { login, checkUser, logout } = useAuth();
 
   const getTasks = async () => {
     setTasks(await db.cget("tasks", ["date", "desc"]));
@@ -75,86 +67,6 @@ export default function App() {
     await db.delete("tasks", id, user);
     await getTasks();
   };
-
-  const providerUrl = "https://eth-rpc-api-testnet.thetatoken.org/rpc";
-  const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
-
-  const login = async () => {
-    try {
-      const providerOptions = {};
-      const web3Modal = new Web3Modal({
-        cacheProvider: true,
-        providerOptions,
-      });
-
-      const provider = await web3Modal.connect();
-      const web3Provider = new Web3Provider(provider);
-      const accounts = await web3Provider.listAccounts();
-      const wallet_address = accounts[0];
-      let identity = JSON.parse(
-        localStorage.getItem(`temp_address:${contractTxId}:${wallet_address}`)
-      );
-      let tx;
-      let err;
-      if (isNil(identity)) {
-        ({ tx, identity, err } = await db.createTempAddress(wallet_address));
-        const linked = await db.getAddressLink(identity.address);
-        console.log(linked);
-        if (isNil(linked)) {
-          alert("something went wrong");
-          return;
-        }
-      } else {
-        localStorage.setItem("temp_address:current", wallet_address);
-        setUser({
-          wallet: wallet_address,
-          privateKey: identity.privateKey,
-        });
-        return;
-      }
-      if (!isNil(tx) && isNil(tx.err)) {
-        identity.tx = tx;
-        identity.linked_address = wallet_address;
-        localStorage.setItem("temp_address:current", wallet_address);
-        localStorage.setItem(
-          `temp_address:${contractTxId}:${wallet_address}`,
-          JSON.stringify(identity)
-        );
-        setUser({
-          wallet: wallet_address,
-          privateKey: identity.privateKey,
-        });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const logout = async () => {
-    if (confirm("Would you like to sign out?")) {
-      await lf.removeItem("temp_address:current");
-      setUser(null, "temp_current");
-    }
-  };
-
-  const checkUser = async () => {
-    const wallet_address = await lf.getItem(`temp_address:current`);
-    if (!isNil(wallet_address)) {
-      const identity = await lf.getItem(
-        `temp_address:${contractTxId}:${wallet_address}`
-      );
-      if (!isNil(identity))
-        setUser({
-          wallet: wallet_address,
-          privateKey: identity.privateKey,
-        });
-    }
-  };
-
-  useEffect(() => {
-    checkUser();
-    setupWeaveDB();
-  }, []);
 
   useEffect(() => {
     if (initDB) {
