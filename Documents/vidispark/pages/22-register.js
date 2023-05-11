ada1cd766461c38a61432378245cf4a5fa00b757
mendsalbert <mendsalbert@gmail.com>
2023-05-11 11:47:30 +0000

    vidispark

--- Documents/vidispark/pages/22-register.js
+++ Documents/vidispark/pages/22-register.js
@@ -98,9 +98,7 @@ const Registration = () => {
             </div>
             <button
               type="button"
-              onChange={
-                onHandleSubmit();
-              }
+              onChange={onHandleSubmit()}
               className="submit-bttn tk-lp-button tk-lp-button--dark tk-lp-w-full"
             >
               Register Now
