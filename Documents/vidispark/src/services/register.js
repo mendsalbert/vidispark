a0d262957499cb801895550ca5aef8c13cfce111
mendsalbert <mendsalbert@gmail.com>
2023-05-11 11:29:35 +0000

    vidispark

---
+++ Documents/vidispark/src/services/register.js
@@ -0,0 +1,24 @@
+// services/todo.js
+
+import { useState, useContext } from "react";
+import { AuthContext } from "../../pages/authContext";
+
+export const useTodo = () => {
+  const { db, user } = useContext(AuthContext);
+
+  const addUser = async (userObj) => {
+    // let res = await db.add(
+    //   {
+    //     task,
+    //     date: db.ts(),
+    //     user_address: db.signer(),
+    //     done: false,
+    //   },
+    //   "tasks",
+    //   user
+    // );
+    console.log(userObj);
+  };
+
+  return { addUser };
+};
