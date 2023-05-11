f9a430dde35d2538ceaecfc3fb4db679d63b8978
mendsalbert <mendsalbert@gmail.com>
2023-05-11 05:21:50 +0000

    vidispark

--- Documents/vidispark/src/auth/index.js
+++ Documents/vidispark/src/auth/index.js
@@ -41,10 +41,11 @@ export const useAuth = () => {
       contractTxId,
     });
     setdb(db);
-    const serializedDb = stringify(db);
+    // const serializedDb = stringify(db);
     // localStorage.setItem("db", serializedDb);
     // await lf.setItem("db", JSON.stringify(db));
-    console.log(serializedDb);
+    const dbString = JSON.stringify(db);
+    console.log(dbString);
     await db.initializeWithoutWallet();
     setInitDB(true);
     localStorage.setItem("initDB", "true");
