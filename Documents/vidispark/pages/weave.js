91c4c74016a0d1b0bc67d95cfae943476154b6ef
mendsalbert <mendsalbert@gmail.com>
2023-05-11 10:49:34 +0000

    vidispark

--- Documents/vidispark/pages/weave.js
+++ Documents/vidispark/pages/weave.js
@@ -10,52 +10,6 @@ export default function App() {
 
   const tabs = isNil(user) ? ["All"] : ["All", "Yours"];
 
-  const getTasks = async () => {
-    setTasks(await db.cget("tasks", ["date", "desc"]));
-  };
-
-  const getMyTasks = async () => {
-    setTasks(
-      await db.cget(
-        "tasks",
-        ["user_address", "==", user.wallet.toLowerCase()],
-        ["date", "desc"]
-      )
-    );
-  };
-
-  const addTask = async (task) => {
-    let res = await db.add(
-      {
-        task,
-        date: db.ts(),
-        user_address: db.signer(),
-        done: false,
-      },
-      "tasks",
-      user
-    );
-
-    await getTasks();
-  };
-
-  const completeTask = async (id) => {
-    await db.update(
-      {
-        done: true,
-      },
-      "tasks",
-      id,
-      user
-    );
-    await getTasks();
-  };
-
-  const deleteTask = async (id) => {
-    await db.delete("tasks", id, user);
-    await getTasks();
-  };
-
   useEffect(() => {
     if (initDB) {
       if (tab === "All") {
