cbac626daa718dc270944df4f625cb87b211075a
mendsalbert <mendsalbert@gmail.com>
2023-05-12 23:34:17 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -5,6 +5,7 @@ const ProfileUploadAndPreview = () => {
   const [uploadUrl, setUploadUrl] = useState("");
   const [videoUrl, setVideoUrl] = useState("");
   const [videoUploading, setVideoUploading] = useState(false);
+  const [uploadStep, setUploadStep] = useState(0);
   const handleFileChange = async (event) => {
     setVideoUploading(true);
     const file = event.target.files[0];
@@ -101,27 +102,18 @@ const ProfileUploadAndPreview = () => {
           <label htmlFor="fileInput2">
             <div className="label">Upload File</div>
             <div className="">
-              {videoUploading ? (
-                <div
-                  class="tw-inline-block tw-h-8 tw-w-8 tw-animate-spin tw-rounded-full tw-border-4 tw-border-solid tw-border-current tw-border-r-transparent tw-align-[-0.125em] tw-motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                  role="status"
-                >
-                  <span class="tw-!absolute tw!-m-px tw!h-px tw!w-px tw!overflow-hidden tw!whitespace-nowrap tw!border-0 tw!p-0 tw![clip:rect(0,0,0,0)]"></span>
+              <div className="upload-box">
+                <svg className="crumina-icon">
+                  <use xlinkHref="#upload-icon" />
+                </svg>
+                <div className="upload-notice">
+                  {" "}
+                  Max 120MB, PNG, JPEG, MP3, MP4
                 </div>
-              ) : (
-                <div className="upload-box">
-                  <svg className="crumina-icon">
-                    <use xlinkHref="#upload-icon" />
-                  </svg>
-                  <div className="upload-notice">
-                    {" "}
-                    Max 120MB, PNG, JPEG, MP3, MP4
-                  </div>
-                  <button className="btn btn-normal btn-dark browse-btn">
-                    Browse File
-                  </button>
-                </div>
-              )}
+                <button className="btn btn-normal btn-dark browse-btn">
+                  Browse File
+                </button>
+              </div>
             </div>
           </label>
         </div>
