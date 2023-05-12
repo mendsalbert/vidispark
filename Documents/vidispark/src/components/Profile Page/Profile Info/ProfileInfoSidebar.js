0c9983dc7a11c028d6c41efd563a57e146e7bd51
mendsalbert <mendsalbert@gmail.com>
2023-05-12 20:30:57 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoSidebar.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoSidebar.js
@@ -8,7 +8,7 @@ const ProfileInfoSidebar = () => {
     <aside>
       <div className="user-db-menu">
         <div className="user-db-header">
-          <div className="user-db-cover-image profile-cover-image" />
+          {/* <div className="user-db-cover-image profile-cover-image" /> */}
           <div className="user-db-cover-image profile-cover-image user-cover-image">
             <img src={`img/content/profile/profile-cover-1.png`} alt="" />
           </div>
