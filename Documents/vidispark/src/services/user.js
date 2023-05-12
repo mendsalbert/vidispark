c4dd4af542d3cbb988222778e51bb4eb3ad2834e
mendsalbert <mendsalbert@gmail.com>
2023-05-12 04:31:37 +0000

    vidispark

--- Documents/vidispark/src/services/user.js
+++ Documents/vidispark/src/services/user.js
@@ -44,7 +44,7 @@ export const useUser = () => {
       user_[0]?.data?.salt
     );
 
-    if (enteredPasswordHash === userObj.password) {
+    if (enteredPasswordHash === user_[0]?.data?.password) {
       // Passwords match, authentication successful
       console.log("Login successful!");
     } else {
