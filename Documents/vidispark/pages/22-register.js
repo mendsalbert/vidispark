0dfc814020257cdc021cc53e46a5da926e6fc474
mendsalbert <mendsalbert@gmail.com>
2023-05-12 04:10:46 +0000

    vidispark

--- Documents/vidispark/pages/22-register.js
+++ Documents/vidispark/pages/22-register.js
@@ -124,7 +124,7 @@ const Registration = () => {
 
             <button
               type="button"
-              className="= tk-lp-w-full tk-lp-tabs-form-item"
+              className=" tk-lp-w-full tk-lp-tabs-form-item"
               data-id="sign-in"
             >
               or
