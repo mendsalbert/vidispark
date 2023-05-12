c94c005c547299ab6c27151330fa62febbd69c09
mendsalbert <mendsalbert@gmail.com>
2023-05-12 21:29:19 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -1,82 +1,38 @@
 import React from "react";
-import {} from "@heroicons/react/outline";
 
 const ProfileUploadAndPreview = () => {
   const categories = [
     {
       name: "Entertainment",
       icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="icon icon-tabler icon-tabler-device-gamepad-2"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <path d="M12 5h3.5a5 5 0 0 1 0 10h-5.5l-4.015 4.227a2.3 2.3 0 0 1 -3.923 -2.035l1.634 -8.173a5 5 0 0 1 4.904 -4.019h3.4z"></path>
-          <path d="M14 15l4.07 4.284a2.3 2.3 0 0 0 3.925 -2.023l-1.6 -8.232"></path>
-          <path d="M8 9v2"></path>
-          <path d="M7 10h2"></path>
-          <path d="M14 10h2"></path>
-        </svg>
+        <ChevronDownIcon className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400" />
       ),
     },
     {
       name: "Music",
       icon: (
-        <svg
-          className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path d="M9 18V5l12-2v13z" />
-        </svg>
+        <ChevronDownIcon className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400" />
       ),
     },
     {
       name: "Education",
       icon: (
-        <svg
-          className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-        </svg>
+        <ChevronDownIcon className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400" />
       ),
     },
     {
       name: "Sports",
       icon: (
-        <svg
-          className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path d="M6 21h12v-4a5 5 0 0 0-5-5H9a5 5 0 0 0-5 5v4z" />
-        </svg>
+        <ChevronDownIcon className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400" />
       ),
     },
     {
       name: "Gaming",
       icon: (
-        <svg
-          className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10-10-4.477-10-10 4.477-10 10-10zm4 13h-3v3h-2v-3H8v-2h3V10h2v3h3v2z" />
-        </svg>
+        <ChevronDownIcon className="tw-w-5 tw-h-5 tw-mr-2 tw-text-gray-400" />
       ),
     },
   ];
-
   return (
     <div className="artwork-upload-box">
       <div className="user-db-title">Upload</div>
