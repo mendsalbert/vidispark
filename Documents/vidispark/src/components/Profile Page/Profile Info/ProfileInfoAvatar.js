1432c3c2bba17839964f455d42709d28f2812037
mendsalbert <mendsalbert@gmail.com>
2023-05-12 10:02:54 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoAvatar.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoAvatar.js
@@ -1,20 +1,6 @@
 import React from "react";
 const ProfileInfoAvatar = () => {
-  return (
-    <div className="upload-photo-box">
-      <div className="user-db-title">Avatar and Cover</div>
-      <div className="user-avatar">
-        <img src="img/content/profile/avatar-upload.png" alt="" />
-      </div>
-      <div className="user-cover-image">
-        <img src="img/content/profile/profile-cover-1.png" alt="" />
-      </div>
-      <div className="upload-notice">
-        Images must be .png or .jpg format. Min size 100x100px (avatar) and
-        1920x320px (cover)
-      </div>
-    </div>
-  );
+  return <></>;
 };
 
 export default ProfileInfoAvatar;
