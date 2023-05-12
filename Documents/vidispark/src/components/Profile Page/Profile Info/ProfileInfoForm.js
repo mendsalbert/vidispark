e39c1355e1f8a928bfdd3f2afb75b5b755dff5e8
mendsalbert <mendsalbert@gmail.com>
2023-05-12 11:34:12 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
@@ -94,7 +94,10 @@ const ProfileInfoForm = () => {
       <div className="upload-photo-box">
         <div className="user-db-title tw-mt-2">Avatar and Cover</div>
         <div className="user-avatar">
-          <img src="img/content/profile/avatar-upload.png" alt="" />
+          <img
+            src="https://i.ibb.co/0C0Dkz1/placeholder-image-person-jpg.jpg"
+            alt=""
+          />
         </div>
         <div className="user-cover-image">
           <img src="img/content/profile/profile-cover-1.png" alt="" />
