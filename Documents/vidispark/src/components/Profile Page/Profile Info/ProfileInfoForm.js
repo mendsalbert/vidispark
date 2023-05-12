fe047e096851bf45ed8ab8453cb0cf0ff45e3de1
mendsalbert <mendsalbert@gmail.com>
2023-05-12 10:02:46 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
@@ -54,6 +54,20 @@ const ProfileInfoForm = () => {
         />
         <span className="input_error-message" id="textareaMessage" />
       </div>
+
+      <div className="upload-photo-box">
+        <div className="user-db-title">Avatar and Cover</div>
+        <div className="user-avatar">
+          <img src="img/content/profile/avatar-upload.png" alt="" />
+        </div>
+        <div className="user-cover-image">
+          <img src="img/content/profile/profile-cover-1.png" alt="" />
+        </div>
+        <div className="upload-notice">
+          Images must be .png or .jpg format. Min size 100x100px (avatar) and
+          1920x320px (cover)
+        </div>
+      </div>
       <button className="btn btn-wide btn-dark" type="submit">
         Save Changes
       </button>
