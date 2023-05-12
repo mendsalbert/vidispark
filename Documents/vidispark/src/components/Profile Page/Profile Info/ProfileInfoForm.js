dc30c9a21452cc26a800dc553af976b47351213b
mendsalbert <mendsalbert@gmail.com>
2023-05-12 12:11:58 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
@@ -22,9 +22,9 @@ const ProfileInfoForm = () => {
       setIsloadingUrl(true);
       const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweENjNEYzZTkxZUVBNmFFRGRBMTA1RmE3QjZDZjA0NzJFQjUxMDdjMGMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODM1NjI0MDYyNzksIm5hbWUiOiJ2aWRpc3BhcmsifQ.4FBhhTMnQ3hY-P-ccuX_jKf-6ml4q6gLG9xIr0a-8Xk"; // Replace with your actual Web3.Storage API token
-      const ext = selectedFile.name.split(".").pop();
+      const ext = selectedFile.name;
       const fileName = `${uuidv4()}.${ext}`;
-
+      console.log(ext);
       const client = new Web3Storage({ token });
       const cid = await client.put([selectedFile], { name: fileName });
       console.log("clicable link", `https://${cid}.ipfs.dweb.link/${fileName}`);
