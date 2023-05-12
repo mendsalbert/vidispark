1daf94ef34369ba6332ae5b58e620e8dabc740e6
mendsalbert <mendsalbert@gmail.com>
2023-05-12 12:03:03 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
@@ -138,9 +138,7 @@ const ProfileInfoForm = () => {
             class="tw-inline-block tw-h-8 tw-w-8 tw-animate-spin tw-rounded-full tw-border-4 tw-border-solid tw-border-current tw-border-r-transparent tw-align-[-0.125em] tw-motion-reduce:animate-[spin_1.5s_linear_infinite]"
             role="status"
           >
-            <span class="tw-!absolute tw!-m-px tw!h-px tw!w-px tw!overflow-hidden tw!whitespace-nowrap tw!border-0 tw!p-0 tw![clip:rect(0,0,0,0)]">
-              Loading...
-            </span>
+            <span class="tw-!absolute tw!-m-px tw!h-px tw!w-px tw!overflow-hidden tw!whitespace-nowrap tw!border-0 tw!p-0 tw![clip:rect(0,0,0,0)]"></span>
           </div>
         ) : (
           <label htmlFor="fileInput">
