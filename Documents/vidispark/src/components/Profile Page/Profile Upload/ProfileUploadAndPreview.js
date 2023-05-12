a29e8cb93c225fa14302902e74c13109da21f1c6
mendsalbert <mendsalbert@gmail.com>
2023-05-12 23:38:43 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -117,12 +117,15 @@ const ProfileUploadAndPreview = () => {
                   Max 120MB, PNG, JPEG, MP3, MP4
                 </div>
                 {videoUploading ? (
-                  <div
-                    class="tw-inline-block tw-h-8 tw-w-8 tw-animate-spin tw-rounded-full tw-border-4 tw-border-solid tw-border-current tw-border-r-transparent tw-align-[-0.125em] tw-motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                    role="status"
-                  >
-                    <span class="tw-!absolute tw!-m-px tw!h-px tw!w-px tw!overflow-hidden tw!whitespace-nowrap tw!border-0 tw!p-0 tw![clip:rect(0,0,0,0)]"></span>
-                  </div>
+                  <>
+                    <div
+                      class="tw-inline-block tw-h-8 tw-w-8 tw-animate-spin tw-rounded-full tw-border-4 tw-border-solid tw-border-current tw-border-r-transparent tw-align-[-0.125em] tw-motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                      role="status"
+                    >
+                      <span class="tw-!absolute tw!-m-px tw!h-px tw!w-px tw!overflow-hidden tw!whitespace-nowrap tw!border-0 tw!p-0 tw![clip:rect(0,0,0,0)]"></span>
+                    </div>
+                    <p className="upload-notice">{uploadStep}</p>
+                  </>
                 ) : (
                   <button className="btn btn-normal btn-dark browse-btn">
                     Browse File
