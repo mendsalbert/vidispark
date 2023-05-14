a0b508c0f1c70dbe7c06980c3187c08385b81825
mendsalbert <mendsalbert@gmail.com>
2023-05-14 04:18:53 +0000

    vidispark

--- Documents/vidispark/src/components/Home/HomeFeaturedItems.js
+++ Documents/vidispark/src/components/Home/HomeFeaturedItems.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { featuredCarousel } from "../../sliderProps";
 import Nft from "../Nft";
