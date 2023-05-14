b6a08c499d656b9c0edb378f4d3e52c9d547453e
mendsalbert <mendsalbert@gmail.com>
2023-05-14 03:58:43 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -9,7 +9,7 @@ const ProfileUploadAndPreview = () => {
   const [videoUploading, setVideoUploading] = useState(false);
   const [uploadStep, setUploadStep] = useState("");
   const [isComplete, setIsComplete] = useState(false);
-  const { addVideo, getAllVideos } = useVideo();
+  const { addVideo } = useVideo();
   const handleFileChange = async (event) => {
     setVideoUploading(true);
     setUploadStep("Uploading Video");
@@ -77,10 +77,6 @@ const ProfileUploadAndPreview = () => {
   };
 
   //states
-
-  useEffect(() => {
-    getAllVideos();
-  }, []);
 
   const [category, setCategory] = useState("");
   const [title, setTitle] = useState("");
