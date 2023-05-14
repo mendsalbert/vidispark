8a3eb6c6f89441f5f5ed493ec8efda0b5853ff34
mendsalbert <mendsalbert@gmail.com>
2023-05-14 05:34:07 +0000

    vidispark

--- Documents/vidispark/src/components/Nft.js
+++ Documents/vidispark/src/components/Nft.js
@@ -6,76 +6,77 @@ import ReactPlayer from "react-player";
 const Nft = ({ data }) => {
   console.log(data);
   return (
-    <div className="featured-item">
-      <div className="featured-item-wrapper">
-        <div className="featured-item-content">
-          <div className="featured-item-image">
-            {/* <Link href="/05-product-page">
-              <a>
-                <picture>
-                  <source type="image/avif" srcSet={data.image} />
-                  <img
-                    src={data.image}
-                    width={304}
-                    height={340}
-                    alt="Featured"
-                  />
-                </picture>
-              </a>
-            </Link> */}
-            <ReactPlayer
-              url="https://media.thetavideoapi.com/video_u3d9u6gy1pmxfrscd9j5csmiam/master.m3u8"
-              controls
-              width="100%"
-              height="auto"
-              className="rounded-md shadow-lg"
-            />
-          </div>
-          <div className="featured-item-info">
-            <div className="title">
-              {" "}
-              <Link href="/05-product-page">
-                <a>{data.videoData.title}</a>
-              </Link>{" "}
-            </div>
-            <div className="tw-flex tw-flex-row tw-items-center tw-justify-between tw-w-full">
-              <span className="tw-flex tw-flex-row tw-items-center tw-space-x-2">
-                <span className="avatar box-26">
-                  {" "}
-                  <Link href="/08-profile-page">
-                    <a>
-                      <picture>
-                        <source
-                          type="image/avif"
-                          srcSet={data.user.avatarUrlL}
-                        />
-                        <img
-                          src={data.user.avatarUrlL}
-                          alt="avatar"
-                          loading="lazy"
-                          width={100}
-                          height={100}
-                        />
-                      </picture>
-                    </a>
-                  </Link>{" "}
-                  <span className="verified">
-                    <svg className="crumina-icon">
-                      <use xlinkHref="#check-icon" />
-                    </svg>{" "}
-                  </span>
-                </span>
-                <span>@{data.user.username}</span>
-              </span>
-              <span className="tw-flex tw-flex-row tw-items-center tw-space-x-1 ">
-                <span>{0}</span>
-                <EyeIcon />
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <></>
+    // <div className="featured-item">
+    //   <div className="featured-item-wrapper">
+    //     <div className="featured-item-content">
+    //       <div className="featured-item-image">
+    //         {/* <Link href="/05-product-page">
+    //           <a>
+    //             <picture>
+    //               <source type="image/avif" srcSet={data.image} />
+    //               <img
+    //                 src={data.image}
+    //                 width={304}
+    //                 height={340}
+    //                 alt="Featured"
+    //               />
+    //             </picture>
+    //           </a>
+    //         </Link> */}
+    //         <ReactPlayer
+    //           url="https://media.thetavideoapi.com/video_u3d9u6gy1pmxfrscd9j5csmiam/master.m3u8"
+    //           controls
+    //           width="100%"
+    //           height="auto"
+    //           className="rounded-md shadow-lg"
+    //         />
+    //       </div>
+    //       <div className="featured-item-info">
+    //         <div className="title">
+    //           {" "}
+    //           <Link href="/05-product-page">
+    //             <a>{data.videoData.title}</a>
+    //           </Link>{" "}
+    //         </div>
+    //         <div className="tw-flex tw-flex-row tw-items-center tw-justify-between tw-w-full">
+    //           <span className="tw-flex tw-flex-row tw-items-center tw-space-x-2">
+    //             <span className="avatar box-26">
+    //               {" "}
+    //               <Link href="/08-profile-page">
+    //                 <a>
+    //                   <picture>
+    //                     <source
+    //                       type="image/avif"
+    //                       srcSet={data.user.avatarUrlL}
+    //                     />
+    //                     <img
+    //                       src={data.user.avatarUrlL}
+    //                       alt="avatar"
+    //                       loading="lazy"
+    //                       width={100}
+    //                       height={100}
+    //                     />
+    //                   </picture>
+    //                 </a>
+    //               </Link>{" "}
+    //               <span className="verified">
+    //                 <svg className="crumina-icon">
+    //                   <use xlinkHref="#check-icon" />
+    //                 </svg>{" "}
+    //               </span>
+    //             </span>
+    //             <span>@{data.user.username}</span>
+    //           </span>
+    //           <span className="tw-flex tw-flex-row tw-items-center tw-space-x-1 ">
+    //             <span>{0}</span>
+    //             <EyeIcon />
+    //           </span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 export default Nft;
