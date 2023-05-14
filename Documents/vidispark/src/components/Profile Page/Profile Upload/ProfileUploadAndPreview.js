880518d7934b9c06e7fd91826e84a9416a64091e
mendsalbert <mendsalbert@gmail.com>
2023-05-14 03:30:25 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NiceSelect from "../../NiceSelect";
 import { useVideo } from "../../../services/videos";
