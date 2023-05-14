e9552a65169dd40865b3831e6fd96d7e98fbc2c0
mendsalbert <mendsalbert@gmail.com>
2023-05-14 21:30:18 +0000

    vidispark

--- Documents/vidispark/pages/21-login.js
+++ Documents/vidispark/pages/21-login.js
@@ -13,8 +13,8 @@ const Login = () => {
 
   const onLoginHandler = () => {
     //!this must be called before the real login start
-    // login();
-    loginUser({ username, password });
+    login();
+    // loginUser({ username, password });
   };
   return (
     <Layouts>
