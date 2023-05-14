fe5257fe5a2fd815acdda0fe0386484a3e09a60f
mendsalbert <mendsalbert@gmail.com>
2023-05-14 08:10:31 +0000

    vidispark

--- Documents/vidispark/src/components/Product/ProductV1/ProductInfo.js
+++ Documents/vidispark/src/components/Product/ProductV1/ProductInfo.js
@@ -32,28 +32,7 @@ const ProductInfo = ({ singleVid }) => {
           <div className="avatar-meta">@DexterStark</div>
         </div>
       </div>
-      <div className="product-meta-section">
-        <div className="product-meta-item">
-          <div className="label">Current bid</div>
-          <div className="value">2.31 ETH</div>
-          <div className="caption">125.230 U$D</div>
-        </div>
-        <div className="product-meta-item">
-          <div className="label">Edition</div>
-          <div className="value">01/40</div>
-          <div className="caption">Tokens</div>
-        </div>
-        <div className="product-meta-item">
-          <div className="label">AUCTION ENDING IN</div>
-          <div className="countdown">
-            <div
-              className="js-countdown"
-              data-timer={104400}
-              data-labels="Days , Hours , Mins , Secs"
-            />
-          </div>
-        </div>
-      </div>
+
       <div className="product-description">
         <div className="small-title">Description</div>
         <p>
