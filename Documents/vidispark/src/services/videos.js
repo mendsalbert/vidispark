fa73a08329f76c988ebf17e5fc27c6d121c85aa1
mendsalbert <mendsalbert@gmail.com>
2023-05-14 03:31:53 +0000

    vidispark

--- Documents/vidispark/src/services/videos.js
+++ Documents/vidispark/src/services/videos.js
@@ -32,8 +32,8 @@ export const useVideo = () => {
 
   const getAllVideos = async () => {
     let res = await db.get("video");
-    return res;
     console.log(res);
+    return res;
     //TODO get videos as well as the user who posted it
     // await db.get("collection_name", "doc_id", "sub_collection_name_1", "sub_doc_id_1", "sub_collection_name_2", "sub_doc_id_2")
   };
