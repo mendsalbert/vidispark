165bee86535a87426e94283f6e965bdb5cbbcede
mendsalbert <mendsalbert@gmail.com>
2023-05-15 20:13:32 +0000

    vidispark

--- Documents/vidispark/pages/10-creators.js
+++ Documents/vidispark/pages/10-creators.js
@@ -14,12 +14,15 @@ const Cretors = ({ getCreator, creators }) => {
   const [state, setstate] = useState([]);
   const [filter_, setFilter_] = useState({});
   const [filteredData, setFilteredData] = useState(null);
+
   useEffect(() => {
     setFilteredData(creators);
   }, [creators]);
+
   const onChnage = (name, value) => {
     setFilter_({ ...filter_, [name]: value });
   };
+
   const onSubmit = (e) => {
     e.preventDefault();
     setFilteredData(getProductByFilter(creators, filter_));
@@ -38,41 +41,7 @@ const Cretors = ({ getCreator, creators }) => {
           </h2>
         </div>
         {/* FILTERABLE BAR */}
-        <div className="filterable-bar">
-          <form id="creators-filter-form" onSubmit={(e) => onSubmit(e)}>
-            <div className="filter-options">
-              <NiceSelect
-                arr={[
-                  { name: "Order by Name (A-Z)", value: "" },
-                  { name: "Order by Last Modification", value: "modification" },
-                  { name: "Order by Registration Date", value: "registration" },
-                ]}
-                ChangeFilterData={(name, value) => onChnage(name, value)}
-                name={"order"}
-              />
 
-              <NiceSelect
-                arr={[
-                  { name: " Verified Creators Only", value: true },
-                  { name: "All Creators", value: "" },
-                ]}
-                ChangeFilterData={(name, value) => onChnage(name, value)}
-                name={"verified"}
-              />
-              <NiceSelect
-                arr={[
-                  { name: " Creators with Active Auctions", value: true },
-                  { name: "All Creators", value: "" },
-                ]}
-                ChangeFilterData={(name, value) => onChnage(name, value)}
-                name={"auction"}
-              />
-            </div>
-            <div className="filter-button">
-              <button className="btn btn-normal btn-dark">Filter</button>
-            </div>
-          </form>
-        </div>
         {/* FILTERABLE BAR */}
         {/* CREATORS GRID */}
         <div className="creators-box grid-4-columns">
