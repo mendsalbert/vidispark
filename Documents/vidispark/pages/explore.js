9f372e07f49ed386eea69e506ad7f622354a3a0a
mendsalbert <mendsalbert@gmail.com>
2023-05-15 18:26:49 +0000

    vidispark

--- Documents/vidispark/pages/explore.js
+++ Documents/vidispark/pages/explore.js
@@ -66,7 +66,7 @@ const Explorev2 = ({
                 onSubmit={(e) => onSubmit(e)}
               >
                 <div className="form-group">
-                  <div className="small-title">🎭 Entertainment</div>
+                  <div className="small-title">Categories</div>
                   <div className="is-two-col-element">
                     <div className="form-list-wrapper">
                       <div className="form-list-elem">
@@ -75,9 +75,9 @@ const Explorev2 = ({
                           name="featured"
                           id="featured"
                           className="cryptoki-checkbox"
-                          onClick={(e) => catagoryfilter("entertainment")}
+                          onClick={(e) => catagoryfilter("featured")}
                         />
-                        <label htmlFor="featured">Music</label>
+                        <label htmlFor="featured">Featured</label>
                       </div>
                       <div className="form-list-elem">
                         <input
