2ae7b933476790f064ea9476d3f7a88c8d006d27
mendsalbert <mendsalbert@gmail.com>
2023-05-16 09:16:52 +0000

    vidispark

--- Documents/vidispark/pages/authContext.js
+++ Documents/vidispark/pages/authContext.js
@@ -75,63 +75,62 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   const login = async () => {
-    // try {
-    // const providerOptions = {};
-    // const web3Modal = new Web3Modal({
-    //   cacheProvider: true,
-    //   providerOptions,
-    // });
+    try {
+      const providerOptions = {};
+      const web3Modal = new Web3Modal({
+        cacheProvider: true,
+        providerOptions,
+      });
 
-    // const provider = await web3Modal.connect();
-    // const web3Provider = new Web3Provider(provider);
-    // const accounts = await web3Provider.listAccounts();
+      const provider = await web3Modal.connect();
+      const web3Provider = new Web3Provider(provider);
+      const accounts = await web3Provider.listAccounts();
 
-    console.log(address);
-    //   const wallet_address = accounts[0];
-    //   let identity = JSON.parse(
-    //     localStorage.getItem(`temp_address:${contractTxId}:${wallet_address}`)
-    //   );
-    //   let tx;
-    //   let err;
-    //   if (isNil(identity)) {
-    //     ({ tx, identity, err } = await db.createTempAddress(wallet_address));
-    //     const linked = await db.getAddressLink(identity.address);
-    //     console.log(linked);
-    //     if (isNil(linked)) {
-    //       alert("something went wrong");
-    //       return;
-    //     }
-    //   } else {
-    //     localStorage.setItem("temp_address:current", wallet_address);
-    //     setUser({
-    //       wallet: wallet_address,
-    //       privateKey: identity.privateKey,
-    //     });
-    //     localStorage.setItem(
-    //       "user",
-    //       JSON.stringify({
-    //         wallet: wallet_address,
-    //         privateKey: identity.privateKey,
-    //       })
-    //     );
-    //     return;
-    //   }
-    //   if (!isNil(tx) && isNil(tx.err)) {
-    //     identity.tx = tx;
-    //     identity.linked_address = wallet_address;
-    //     localStorage.setItem("temp_address:current", wallet_address);
-    //     localStorage.setItem(
-    //       `temp_address:${contractTxId}:${wallet_address}`,
-    //       JSON.stringify(identity)
-    //     );
-    //     setUser({
-    //       wallet: wallet_address,
-    //       privateKey: identity.privateKey,
-    //     });
-    //   }
-    // } catch (error) {
-    //   console.error(error);
-    // }
+      const wallet_address = accounts[0];
+      let identity = JSON.parse(
+        localStorage.getItem(`temp_address:${contractTxId}:${wallet_address}`)
+      );
+      let tx;
+      let err;
+      if (isNil(identity)) {
+        ({ tx, identity, err } = await db.createTempAddress(wallet_address));
+        const linked = await db.getAddressLink(identity.address);
+        console.log(linked);
+        if (isNil(linked)) {
+          alert("something went wrong");
+          return;
+        }
+      } else {
+        localStorage.setItem("temp_address:current", wallet_address);
+        setUser({
+          wallet: wallet_address,
+          privateKey: identity.privateKey,
+        });
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            wallet: wallet_address,
+            privateKey: identity.privateKey,
+          })
+        );
+        return;
+      }
+      if (!isNil(tx) && isNil(tx.err)) {
+        identity.tx = tx;
+        identity.linked_address = wallet_address;
+        localStorage.setItem("temp_address:current", wallet_address);
+        localStorage.setItem(
+          `temp_address:${contractTxId}:${wallet_address}`,
+          JSON.stringify(identity)
+        );
+        setUser({
+          wallet: wallet_address,
+          privateKey: identity.privateKey,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const logout = async () => {
