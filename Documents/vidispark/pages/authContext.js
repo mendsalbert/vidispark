6b6419a5ff6579d41208a3aae96c3fb8d9b2c961
mendsalbert <mendsalbert@gmail.com>
2023-05-16 10:29:55 +0000

    vidispark

--- Documents/vidispark/pages/authContext.js
+++ Documents/vidispark/pages/authContext.js
@@ -75,91 +75,89 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   const login = async () => {
-    if (db) {
-      try {
-        console.log(address);
-        const providerOptions = {};
-        const web3Modal = new Web3Modal({
-          cacheProvider: true,
-          providerOptions,
-        });
+    try {
+      console.log(address);
+      const providerOptions = {};
+      const web3Modal = new Web3Modal({
+        cacheProvider: true,
+        providerOptions,
+      });
 
-        const provider = await web3Modal.connect();
-        const web3Provider = new Web3Provider(provider);
-        const accounts = await web3Provider.listAccounts();
+      const provider = await web3Modal.connect();
+      const web3Provider = new Web3Provider(provider);
+      const accounts = await web3Provider.listAccounts();
 
-        const wallet_address = accounts[0];
-        console.log("wallet_address", wallet_address);
-        let identity = JSON.parse(
-          localStorage.getItem(`temp_address:${contractTxId}:${wallet_address}`)
-        );
-        let tx;
-        let err;
-        if (isNil(identity)) {
-          console.log("calling db.createTempAddress");
-          ({ tx, identity, err } = await db?.(wallet_address));
-          const linked = await db.getAddressLink(identity.address);
-          console.log(linked);
-          if (isNil(linked)) {
-            alert("something went wrong");
-            return;
-          }
-        } else {
-          localStorage.setItem("temp_address:current", wallet_address);
-          setUser({
-            wallet: wallet_address,
-            privateKey: identity.privateKey,
-          });
-          localStorage.setItem(
-            "user",
-            JSON.stringify({
-              wallet: wallet_address,
-              privateKey: identity.privateKey,
-            })
-          );
+      const wallet_address = accounts[0];
+      console.log("wallet_address", wallet_address);
+      let identity = JSON.parse(
+        localStorage.getItem(`temp_address:${contractTxId}:${wallet_address}`)
+      );
+      let tx;
+      let err;
+      if (isNil(identity)) {
+        console.log("calling db.createTempAddress");
+        ({ tx, identity, err } = await db?.(wallet_address));
+        const linked = await db.getAddressLink(identity.address);
+        console.log(linked);
+        if (isNil(linked)) {
+          alert("something went wrong");
           return;
         }
-        if (!isNil(tx) && isNil(tx.err)) {
-          identity.tx = tx;
-          identity.linked_address = wallet_address;
-          localStorage.setItem("temp_address:current", wallet_address);
-          localStorage.setItem(
-            `temp_address:${contractTxId}:${wallet_address}`,
-            JSON.stringify(identity)
-          );
-          setUser({
+      } else {
+        localStorage.setItem("temp_address:current", wallet_address);
+        setUser({
+          wallet: wallet_address,
+          privateKey: identity.privateKey,
+        });
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
             wallet: wallet_address,
             privateKey: identity.privateKey,
-          });
-        }
-      } catch (error) {
-        console.error(error);
+          })
+        );
+        return;
       }
+      if (!isNil(tx) && isNil(tx.err)) {
+        identity.tx = tx;
+        identity.linked_address = wallet_address;
+        localStorage.setItem("temp_address:current", wallet_address);
+        localStorage.setItem(
+          `temp_address:${contractTxId}:${wallet_address}`,
+          JSON.stringify(identity)
+        );
+        setUser({
+          wallet: wallet_address,
+          privateKey: identity.privateKey,
+        });
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
-
-  const logout = async () => {
-    if (confirm("Would you like to sign out?")) {
-      await lf.removeItem("temp_address:current");
-      setUser(null, "temp_current");
-      localStorage.removeItem("user", "temp_current");
-    }
-  };
-
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        db,
-        initDB,
-        login,
-        logout,
-        checkUser,
-        setupWeaveDB,
-        userInfo,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
 };
+
+const logout = async () => {
+  if (confirm("Would you like to sign out?")) {
+    await lf.removeItem("temp_address:current");
+    setUser(null, "temp_current");
+    localStorage.removeItem("user", "temp_current");
+  }
+};
+
+return (
+  <AuthContext.Provider
+    value={{
+      user,
+      db,
+      initDB,
+      login,
+      logout,
+      checkUser,
+      setupWeaveDB,
+      userInfo,
+    }}
+  >
+    {children}
+  </AuthContext.Provider>
+);
