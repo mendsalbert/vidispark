af3235091d32f9c80d2558f9769726947257a1b8
mendsalbert <mendsalbert@gmail.com>
2023-05-17 10:40:56 +0000

    vidispark

---
+++ Documents/vidispark/utils/contract/index.js
@@ -0,0 +1,19 @@
+import { ethers } from "ethers";
+import XDCPayABI from "./XDCPay.json";
+
+export const contract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+
+  if (ethereum) {
+    const signer = provider.getSigner();
+
+    const contractReader = new ethers.Contract(
+      "0x85b2FD5f1e413168f131A41EB0c8b858642314c2",
+      XDCPayABI.abi,
+      signer
+    );
+
+    return contractReader;
+  }
+};
