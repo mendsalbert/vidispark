7ec93793e1319657786d6f3f2ee8be83e7259599
mendsalbert <mendsalbert@gmail.com>
2023-05-19 20:57:33 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/AIpage.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/AIpage.js
@@ -76,58 +76,6 @@ const AIpage = () => {
   return (
     <div className="cryptoki-form" id="personal-info-form">
       <div className="user-db-title">Vidispark AI</div>
-      <div className="form-group">
-        <div className="form-field">
-          <label htmlFor="name">Full name</label>
-          <input
-            type="text"
-            id="name"
-            defaultValue={userInfo[0]?.data?.fullname}
-            onChange={(e) => {
-              setFullname(e.target.value);
-            }}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <div className="form-field">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-            defaultValue={userInfo[0]?.data?.username}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="phone"> contact</label>
-          <input
-            type="number"
-            id="phone"
-            defaultValue={parseInt(userInfo[0]?.data?.contact)}
-            value={contact}
-            onChange={(e) => {
-              setContact(e.target.value);
-            }}
-          />
-        </div>
-      </div>
-
-      <div className="form-group">
-        <div className="form-field">
-          <label htmlFor="username">Email</label>
-          <input
-            type="email"
-            id="username"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-            defaultValue={userInfo[0]?.data?.email}
-          />
-        </div>
-      </div>
 
       <div className="form-field ">
         <label htmlFor="message">Bio</label>
@@ -149,122 +97,14 @@ const AIpage = () => {
         />
         <span className="input_error-message" id="textareaMessage" />
       </div>
-      <div className="user-db-title tw-pt-3">Monetization(Market Place)</div>
-      <div className="form-group">
-        <div className="form-field">
-          <label htmlFor="name">Market Bio</label>
-          <input
-            type="text"
-            id="name"
-            defaultValue={userInfo[0]?.data?.marketBio}
-            onChange={(e) => {
-              setMarketBio(e.target.value);
-            }}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <div className="form-field">
-          <label htmlFor="username">Market Price / hour</label>
-          <input
-            type="text"
-            id="username"
-            onChange={(e) => {
-              setMarketPrice(e.target.value);
-            }}
-            defaultValue={userInfo[0]?.data?.marketPrice}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="username">Previous Brand(s) worked with</label>
-          <input
-            type="text"
-            id="username"
-            onChange={(e) => {
-              setPrevbrands(e.target.value);
-            }}
-            defaultValue={userInfo[0]?.data?.prevBrands}
-          />
-        </div>
-      </div>
 
-      <div className="upload-photo-box">
-        <div className="user-db-title tw-mt-2">Avatar and Cover</div>
-        <input
-          type="file"
-          id="fileInput"
-          className="tw-hidden"
-          onChange={(e) => handleFileChange(e, "avatar")}
-        />
-        {isLoadingUrl ? (
-          <div
-            class="tw-inline-block tw-h-8 tw-w-8 tw-animate-spin tw-rounded-full tw-border-4 tw-border-solid tw-border-current tw-border-r-transparent tw-align-[-0.125em] tw-motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status"
-          >
-            <span class="tw-!absolute tw!-m-px tw!h-px tw!w-px tw!overflow-hidden tw!whitespace-nowrap tw!border-0 tw!p-0 tw![clip:rect(0,0,0,0)]"></span>
-          </div>
-        ) : (
-          <label htmlFor="fileInput">
-            <div className="user-avatar">
-              <img
-                src={`${
-                  urlReady
-                    ? url
-                    : userInfo[0]?.data?.avatarUrl?.length > 1
-                    ? userInfo[0]?.data?.avatarUrl
-                    : "https://i.ibb.co/0C0Dkz1/placeholder-image-person-jpg.jpg"
-                } `}
-                alt=""
-              />
-            </div>
-          </label>
-        )}
-
-        <input
-          type="file"
-          id="fileInput2"
-          className="tw-hidden"
-          onChange={(e) => handleFileChange(e, "cover")}
-        />
-
-        {isCoverLoading ? (
-          <div
-            class="tw-inline-block tw-h-8 tw-w-8 tw-animate-spin tw-rounded-full tw-border-4 tw-border-solid tw-border-current tw-border-r-transparent tw-align-[-0.125em] tw-motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status"
-          >
-            <span class="tw-!absolute tw!-m-px tw!h-px tw!w-px tw!overflow-hidden tw!whitespace-nowrap tw!border-0 tw!p-0 tw![clip:rect(0,0,0,0)]"></span>
-          </div>
-        ) : (
-          <label htmlFor="fileInput2">
-            <div className="user-cover-image">
-              <img
-                src={`${
-                  coverReady
-                    ? coverUrl
-                    : userInfo[0]?.data?.avatarCover?.length > 1
-                    ? userInfo[0]?.data?.avatarCover
-                    : "img/content/profile/profile-cover-1.png"
-                } `}
-                alt=""
-              />
-            </div>
-          </label>
-        )}
-        {/* <div className="user-cover-image">
-          <img src="img/content/profile/profile-cover-1.png" alt="" />
-        </div> */}
-        <div className="upload-notice">
-          Images must be .png or .jpg format. Min size 100x100px (avatar) and
-          1920x320px (cover)
-        </div>
-      </div>
       <button
         onClick={() => {
-          onUpdateUserDataHandler();
+          //   onUpdateUserDataHandler();
         }}
         className="btn btn-wide btn-dark tw-mt-3"
       >
-        Save Changes
+        Generate{" "}
       </button>
     </div>
   );
