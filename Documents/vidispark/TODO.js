d1e8ffdc4496e052efae456a6d42cb726a6abaef
mendsalbert <mendsalbert@gmail.com>
2023-05-19 18:58:34 +0000

    vidispark

--- Documents/vidispark/TODO.js
+++ Documents/vidispark/TODO.js
@@ -2,12 +2,14 @@
  * TODO
  * leaderboard xx
  * spotlight xx
+ * metaverse xx
  * AI for generating caption for video
- * metaverse
  * work on footer and links
  * check for mobile view
  * demo video
  * a very rich documentation (white paper) add link to footer
- * videos created by a user
- * likes gotten by a user
+ * videos created by a user xx
+ * likes gotten by a user  xx
+ * validation and aleart etc
+ * handle authentication very well
  */
