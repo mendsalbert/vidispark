6104ce830e619fe9372ffa2f21e349736bb169b5
mendsalbert <mendsalbert@gmail.com>
2023-05-19 04:26:49 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/ProfileHeader.js
+++ Documents/vidispark/src/components/Profile Page/ProfileHeader.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-const ProfileHeader = () => {
+const ProfileHeader = ({ user }) => {
   return (
     <div className="profile-header-section">
       <div className="cover-image">
