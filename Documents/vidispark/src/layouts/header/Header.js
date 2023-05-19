01dc3c54f693f4420f7eacef8830d3c0450abd28
mendsalbert <mendsalbert@gmail.com>
2023-05-19 03:58:35 +0000

    vidispark

--- Documents/vidispark/src/layouts/header/Header.js
+++ Documents/vidispark/src/layouts/header/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
     fetchBal();
   }, []);
 
-  console.log(balance.toString());
+  console.log(balance?.toString());
   return (
     <header className="site-header">
       <div className="topbar padding-top-bottom border-bottom">
