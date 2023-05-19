3939a104275b0b8862a829c8e3281864e4f228b3
mendsalbert <mendsalbert@gmail.com>
2023-05-19 18:20:05 +0000

    vidispark

--- Documents/vidispark/src/components/Product/ProductV1/ProductTabs.js
+++ Documents/vidispark/src/components/Product/ProductV1/ProductTabs.js
@@ -101,7 +101,69 @@ const ProductTabs = ({ filteredVideos }) => {
       {/*/TAB BIDS*/}
       {/*TAB DETAILS*/}
       <div id="tab2" className={`tab ${activeContent("tab2")}`}>
-        {/*  */}
+        {filteredVideos.map((data) => (
+          <div className="featured-item">
+            <div className="featured-item-wrapper">
+              <div className="featured-item-content">
+                <div className="featured-item-image">
+                  <Link href={`/video/${data?.videoId}`}>
+                    <iframe
+                      src={data?.videoData?.videoUrl}
+                      border="0"
+                      width="100%"
+                      height="100%"
+                      allowfullscreen
+                      allow="autoplay; fullscreen"
+                      autoplay="false"
+                    />
+                  </Link>
+                </div>
+                <div className="featured-item-info">
+                  <div
+                    className="title"
+                    onClick={() => {
+                      updateCount(data?.videoId);
+                    }}
+                  >
+                    {" "}
+                    <Link href={`/video/${data?.videoId}`}>
+                      <a>{data?.videoData?.title}</a>
+                    </Link>{" "}
+                  </div>
+                  <div className="tw-flex tw-flex-row tw-items-center tw-justify-between tw-w-full">
+                    <span className="tw-flex tw-flex-row tw-items-center tw-space-x-2">
+                      <span className="avatar box-26">
+                        {" "}
+                        <Link href={`/profile-page/${data?.userId}`}>
+                          <a>
+                            <picture>
+                              <source
+                                type="image/avif"
+                                srcSet={data?.user?.avatarUrl}
+                              />
+                              <img
+                                src={data?.user?.avatarUrl}
+                                alt="avatar"
+                                loading="lazy"
+                                width={100}
+                                height={100}
+                              />
+                            </picture>
+                          </a>
+                        </Link>
+                      </span>
+                      <span>@{data?.user?.username}</span>
+                    </span>
+                    <span className="tw-flex tw-flex-row tw-items-center tw-space-x-1 ">
+                      <span>{data?.videoData?.views}</span>
+                      <EyeIcon />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
