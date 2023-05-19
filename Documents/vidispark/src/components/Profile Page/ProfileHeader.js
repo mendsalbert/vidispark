003b20302b5e6186859a7d9295dfbd4c324a5283
mendsalbert <mendsalbert@gmail.com>
2023-05-19 04:30:30 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/ProfileHeader.js
+++ Documents/vidispark/src/components/Profile Page/ProfileHeader.js
@@ -5,7 +5,7 @@ const ProfileHeader = ({ user }) => {
   return (
     <div className="profile-header-section">
       <div className="cover-image">
-        <img src={user?.avatarCover} alt="" className="tw-w-full tw-h-30" />
+        <img src={user?.avatarCover} alt="" className="tw-w-full tw-h-28" />
       </div>
       <div className="about-author-section container">
         <div className="profile-header-user-profile">
