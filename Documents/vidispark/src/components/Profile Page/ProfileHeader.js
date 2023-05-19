53e932df9e5fdadc693d7057c2d5b4739a19e605
mendsalbert <mendsalbert@gmail.com>
2023-05-19 18:42:38 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/ProfileHeader.js
+++ Documents/vidispark/src/components/Profile Page/ProfileHeader.js
@@ -28,12 +28,7 @@ const ProfileHeader = ({ user }) => {
         </div>
         <div className="author-primary-info">
           <div className="activity-meta">
-            <div className="followers-number">
-              {/* <div className="number">
-                {user?.followers?.length < 1 ? 0 : user?.followers?.length}
-              </div>
-              <div className="label">Followers</div> */}
-            </div>
+            <div className="followers-number"></div>
             <div className="following-number">
               {/* <div className="number">
                 {" "}
@@ -58,6 +53,11 @@ const ProfileHeader = ({ user }) => {
               <svg className="crumina-icon tw-ml-2">
                 <use xlinkHref="#copy-icon" />
               </svg>
+
+              <div className="number">
+                {user?.followers?.length < 1 ? 0 : user?.followers?.length}
+              </div>
+              <div className="label">Followers</div>
             </div>
           </div>
           <div className="projects-meta">
