e580b6b7b3b3e4ec77c6385a850deb35f2d74700
mendsalbert <mendsalbert@gmail.com>
2023-05-19 22:13:59 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/AIpage.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/AIpage.js
@@ -12,7 +12,7 @@ const AIpage = () => {
   });
 
   const openai = new OpenAIApi(configuration);
-  console.log(process.env.GREETING);
+  console.log(process.env.OPEN_AI_KEY);
 
   const generateContent = async () => {
     const completion = await openai.createCompletion({
