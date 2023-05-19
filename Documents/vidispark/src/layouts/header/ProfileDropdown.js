8e5c5038928c44f6bb27b2172578d7013517ee2b
mendsalbert <mendsalbert@gmail.com>
2023-05-19 04:06:40 +0000

    vidispark

--- Documents/vidispark/src/layouts/header/ProfileDropdown.js
+++ Documents/vidispark/src/layouts/header/ProfileDropdown.js
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import useClickOutside from "../../useClickOutside";
 import { useUser } from "../../services/user";
 
-const ProfileDropdown = ({ address }) => {
+const ProfileDropdown = ({}) => {
   const [toggle, setToggle] = useState(false);
   const dayMood = useRef(true);
   let domNode = useClickOutside(() => {
@@ -45,7 +45,7 @@ const ProfileDropdown = ({ address }) => {
       {/*user-meta*/}
       <div className="user-meta" onClick={() => setToggle(!toggle)}>
         <div className="user_name">{user?.fullname}</div>
-        <div className="user_score">{etherValue} VDPS</div>
+        <div className="user_score">{} VDPS</div>
       </div>
       {/*/user-meta*/}
       {/*user-avatar*/}
