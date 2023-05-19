bc9f3581efb6ae9cb449344a3e4331ac84664673
mendsalbert <mendsalbert@gmail.com>
2023-05-19 20:41:05 +0000

    vidispark

--- Documents/vidispark/next.config.js
+++ Documents/vidispark/next.config.js
@@ -3,11 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const nextConfig = {
   reactStrictMode: true,
-
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
-  },
 };
 
 module.exports = nextConfig;
