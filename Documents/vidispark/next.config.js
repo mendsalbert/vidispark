9e514fc5dd4fbda15f863e68ecf24b8547e5aca1
mendsalbert <mendsalbert@gmail.com>
2023-05-19 22:24:21 +0000

    vidispark

--- Documents/vidispark/next.config.js
+++ Documents/vidispark/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   env: {
     OPEN_AI_KEY: "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",
     GREETING: "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",
-    TEST: "test",
+    TEST: "testfdf",
   },
 };
 
