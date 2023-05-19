a50cdf0a1f91bd48a6de19657eccd5e6518d9a22
mendsalbert <mendsalbert@gmail.com>
2023-05-19 18:10:26 +0000

    vidispark

--- Documents/vidispark/pages/video/[id].js
+++ Documents/vidispark/pages/video/[id].js
@@ -14,7 +14,7 @@ const Product05 = () => {
   const { videoResults } = useVideo();
   const singleVid = videoResults?.filter((item) => item.videoId == id);
   const filteredVideos = videoResults.filter(
-    (video) => video.user.userId === id
+    (video) => video?.user?.userId === id
   );
   console.log(filteredVideos);
 
