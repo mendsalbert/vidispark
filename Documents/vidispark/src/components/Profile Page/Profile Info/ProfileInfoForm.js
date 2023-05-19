5e59cf073bb35e0576b3f2f0ca97994f9eacc3bf
mendsalbert <mendsalbert@gmail.com>
2023-05-19 19:55:52 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
@@ -17,7 +17,7 @@ const ProfileInfoForm = () => {
         <input
           id="message"
           name="message"
-          className="tw-w-full tw-rounded-full tw-ring-1 tw-py-4"
+          className="tw-w-full tw-rounded-full tw-ring-1 tw-py-4 tw-px-3"
           cols={20}
           rows={10}
           // value={bio}
