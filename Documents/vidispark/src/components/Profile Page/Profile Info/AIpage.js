0e9da614d67b16801e2ff24f54518f356c5e7a54
mendsalbert <mendsalbert@gmail.com>
2023-05-19 22:48:24 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/AIpage.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/AIpage.js
@@ -58,7 +58,7 @@ const AIpage = () => {
         />
         <span className="input_error-message" id="textareaMessage" />
       </div>
-      <p htmlFor="name" className="tw-mb-8 tw-ring-2">
+      <p htmlFor="name" className="tw-mb-8 tw-ring-2 rounded-full">
         {res && res}{" "}
       </p>
 
