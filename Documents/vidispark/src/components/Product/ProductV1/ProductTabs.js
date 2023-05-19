a70d28bd60e64f066905c7bd1beed95183f96828
mendsalbert <mendsalbert@gmail.com>
2023-05-19 18:34:05 +0000

    vidispark

--- Documents/vidispark/src/components/Product/ProductV1/ProductTabs.js
+++ Documents/vidispark/src/components/Product/ProductV1/ProductTabs.js
@@ -29,79 +29,7 @@ const ProductTabs = ({ filteredVideos }) => {
       </ul>
       {/*/TAB TITLE*/}
       {/*TAB BIDS*/}
-      <div id="tab1" className={`tab ${activeContent("tab1")}`}>
-        <div className="tab-content">
-          <div className="placed-bid">
-            <div className="bid-placer">
-              <input className="tw-bg-white dark:tw-bg-red-400" />
-            </div>
-            <div className="bid-box">
-              <div className="crypto-value">2.31 ETH</div>
-              <div className="currency-value">125.230 U$D</div>
-            </div>
-          </div>
 
-          {/* comment */}
-          <div className="placed-bid">
-            <div className="bid-placer">
-              <div className="avatar box-42">
-                {" "}
-                <Link href="/08-profile-page">
-                  <a>
-                    <img src="img/avatar-4.png" alt="avatar" />
-                  </a>
-                </Link>{" "}
-              </div>
-              <div className="bid-info">
-                <div className="bid-title">
-                  Bid placed by{" "}
-                  <span className="gradient-text">
-                    {" "}
-                    <Link href="/08-profile-page">
-                      <a>Jenn Art</a>
-                    </Link>{" "}
-                  </span>
-                </div>
-                <div className="bid-date">03/25/2021 at 12:19pm</div>
-              </div>
-            </div>
-            <div className="bid-box">
-              <div className="crypto-value">2.31 ETH</div>
-              <div className="currency-value">125.230 U$D</div>
-            </div>
-          </div>
-
-          <div className="placed-bid">
-            <div className="bid-placer">
-              <div className="avatar box-42">
-                {" "}
-                <Link href="/08-profile-page">
-                  <a>
-                    <img src="img/avatar-5.png" alt="avatar" />
-                  </a>
-                </Link>{" "}
-              </div>
-              <div className="bid-info">
-                <div className="bid-title">
-                  Bid placed by{" "}
-                  <span className="gradient-text">
-                    {" "}
-                    <Link href="/08-profile-page">
-                      <a>Derek Greyson</a>
-                    </Link>{" "}
-                  </span>
-                </div>
-                <div className="bid-date">03/25/2021 at 12:19pm</div>
-              </div>
-            </div>
-            <div className="bid-box">
-              <div className="crypto-value">2.31 ETH</div>
-              <div className="currency-value">125.230 U$D</div>
-            </div>
-          </div>
-          {/*/Bid Item*/}
-        </div>
-      </div>
       {/*/TAB BIDS*/}
       {/*TAB DETAILS*/}
       <div id="tab2" className={`tab ${activeContent("tab2")}`}>
