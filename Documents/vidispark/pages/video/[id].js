f7ee341ac7f1b7b0e5de1c5f3d1d3518a9ec918b
mendsalbert <mendsalbert@gmail.com>
2023-05-19 18:13:41 +0000

    vidispark

--- Documents/vidispark/pages/video/[id].js
+++ Documents/vidispark/pages/video/[id].js
@@ -13,10 +13,7 @@ const Product05 = () => {
 
   const { videoResults } = useVideo();
   const singleVid = videoResults?.filter((item) => item.videoId == id);
-  const filteredVideos = videoResults.filter(
-    (video) => video?.uploaderId === id
-  );
-  console.log(singleVid);
+  const filteredVideos = videoResults.filter((item) => console.log(item));
   console.log(filteredVideos);
 
   //TODO: 2. work on the follow button 3. work on the like button 4. comments 5. others videos from creator
