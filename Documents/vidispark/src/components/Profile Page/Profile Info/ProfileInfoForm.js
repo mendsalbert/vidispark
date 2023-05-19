48b21a90798091b086eb612b338956522bf3cc9e
mendsalbert <mendsalbert@gmail.com>
2023-05-19 20:26:58 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
+++ Documents/vidispark/src/components/Profile Page/Profile Info/ProfileInfoForm.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useUser } from "../../../services/user";
-import { Web3Storage } from "web3.storage";
-import { v4 as uuidv4 } from "uuid";
+
+import { Configuration, OpenAIApi } from "openai";
 
 const ProfileInfoForm = () => {
   const { updateUser, userInfo } = useUser();
+  const configuration = new Configuration({
+    organization: "org-iW0tOES3m75oHB2cx9IxyB8I",
+    apiKey: "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",
+  });
 
+  const openai = new OpenAIApi(configuration);
+  const generateContent = async () => {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `A student wants to learn about a , generate 6 modules that a student can use to learn as . A module consists of a title and a description, separated by a colon.`,
+      temperature: 1.4,
+      top_p: 0.7,
+      max_tokens: 120,
+    });
+    return parseString(completion.data.choices[0]?.text);
+  };
   return (
     <div className="cryptoki-form" id="personal-info-form">
       <div className="user-db-title">Vidispark AI</div>
