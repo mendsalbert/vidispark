c348632e6828dcaa89ff6106b2fa97010578b01d
mendsalbert <mendsalbert@gmail.com>
2023-05-21 06:22:05 +0000

    vidispark

--- Documents/vidispark/pages/profile-page/[id].js
+++ Documents/vidispark/pages/profile-page/[id].js
@@ -17,6 +17,126 @@ const Profile = () => {
     countdown();
   }, []);
 
-  return <Layouts></Layouts>;
+  return (
+    <Layouts>
+      <div className="primary-content-area profile-page">
+        {/*  PROFILE HEADER SECTION */}
+        <ProfileHeader user={user[0]?.data} />
+        {/*  PROFILE HEADER SECTION */}
+        {/*  MOBILE PROFILE HEADER SECTION */}
+        <div className="profile-header-mobile">
+          <div className="cover-image">
+            <img src={user[0]?.data?.avatarCover} alt="" />
+          </div>
+          <div className="about-author-section container">
+            <div className="profile-header-user-profile">
+              <div className="avatar box-64 bordered">
+                <Link href="/08-profile-page">
+                  <a>
+                    <img src={user[0]?.data?.avatarUrl} alt="" />
+                  </a>
+                </Link>
+                <span className="verified">
+                  <svg className="crumina-icon">
+                    <use xlinkHref="#check-icon" />
+                  </svg>
+                </span>
+              </div>
+              <div className="profile-author-info">
+                <h5 className="author-name gradient-text">
+                  <Link href="/08-profile-page">
+                    <a>{user[0]?.data?.fullname}</a>
+                  </Link>
+                </h5>
+                <div className="author-meta">@{user[0]?.data?.fullname}</div>
+                <div className="download-file">
+                  {user[0]?.data?.user_address?.slice(0, 6) +
+                    "..." +
+                    user[0]?.data?.user_address?.slice(-4)}
+                  <svg className="crumina-icon">
+                    <use xlinkHref="#copy-icon" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className="follow-container">
+              <div className="more-link-wrapper"></div>
+              <div className="follow-box">
+                {/* <button
+                  type="button"
+                  onClick={() => {
+                    setFollowerState("nf");
+                    followUser(singleVid?.videoData?.uploaderId);
+                  }}
+                  className="btn btn-wide btn-dark "
+                >
+                  {followerState == "nf" ? "Following" : " Follow"}
+                </button> */}
+              </div>
+            </div>
+          </div>
+          <ul className="social-icons-list">
+            <li className="social-icon icon-border">
+              <a href="#">
+                <svg className="crumina-icon">
+                  <use xlinkHref="#link-icon" />
+                </svg>
+              </a>
+            </li>
+            <li className="social-icon icon-border">
+              <a href="#">
+                <svg className="crumina-icon">
+                  <use xlinkHref="#facebook-icon" />
+                </svg>
+              </a>
+            </li>
+            <li className="social-icon icon-border">
+              <a href="#">
+                <svg className="crumina-icon">
+                  <use xlinkHref="#twitter-icon" />
+                </svg>
+              </a>
+            </li>
+            <li className="social-icon icon-border">
+              <a href="#">
+                <svg className="crumina-icon">
+                  <use xlinkHref="#instagram-icon" />
+                </svg>
+              </a>
+            </li>
+            <li className="social-icon icon-border">
+              <a href="#">
+                <svg className="crumina-icon">
+                  <use xlinkHref="#twitch-icon" />
+                </svg>
+              </a>
+            </li>
+          </ul>
+          <div className="author-primary-info">
+            <div className="activity-meta">
+              <div className="followers-number">
+                <div className="number">
+                  {" "}
+                  {user[0]?.data?.followers < 1 ? 0 : user[0]?.data?.followers}
+                </div>
+                <div className="label">Followers</div>
+              </div>
+              <div className="following-number">
+                <div className="number">
+                  {" "}
+                  {user[0]?.data?.following < 1 ? 0 : user[0]?.data?.following}
+                </div>
+                <div className="label">Following</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*  MOBILE PROFILE HEADER SECTION */}
+        {/*  TABS BLOCK */}
+        <ProfilePageTabs id={id} />
+        {/*  TABS BLOCK */}
+      </div>
+    </Layouts>
+  );
 };
 export default Profile;
