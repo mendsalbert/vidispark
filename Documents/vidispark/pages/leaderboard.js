a1a2a15fc5e8f82d601566f81eebe59cfa5484ca
mendsalbert <mendsalbert@gmail.com>
2023-05-21 09:58:28 +0000

    vidispark

--- Documents/vidispark/pages/leaderboard.js
+++ Documents/vidispark/pages/leaderboard.js
@@ -39,8 +39,12 @@ const Cretors11 = ({ getCollector, collectors }) => {
   return (
     <Layouts>
       <div className="primary-content-area container content-padding">
-        <div className="user-db-title">Personal Info</div>
-
+        <div className="page-title-section">
+          <h2>
+            Explore
+            <span className="gradient-text"> Users with more tokens</span>
+          </h2>
+        </div>
         {/* FILTERABLE BAR */}
 
         {/* FILTERABLE BAR */}
