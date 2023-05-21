149efc83ec1a4a03e244cff9952ba30cf512a25a
mendsalbert <mendsalbert@gmail.com>
2023-05-21 04:15:00 +0000

    vidispark

--- Documents/vidispark/pages/29-account-settings.js
+++ Documents/vidispark/pages/29-account-settings.js
@@ -1,9 +1,29 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layouts from "../src/layouts/Layouts";
 import ProfileInfoSidebar from "../src/components/Profile Page/Profile Info/ProfileInfoSidebar";
 
 const AccountSettings = () => {
+  const [facebook, setFacebook] = useState("");
+  const [twitter, setTwitter] = useState("");
+  const [instagram, setInstagram] = useState("");
+  const [twitch, setTwittwitch] = useState("");
+  const [youtube, setYoutube] = useState("");
+  const [discord, setDiscord] = useState("");
+
+  const onUpdateUserDataHandler = () => {
+    const updatedUserData = {
+      facebook: facebook !== "" ? facebook : userInfo[0]?.data?.facebook,
+      twitter: twitter !== "" ? twitter : userInfo[0]?.data?.twitter,
+      instagram: instagram !== "" ? instagram : userInfo[0]?.data?.instagram,
+      twitch: twitch !== "" ? twitch : userInfo[0]?.data?.twitch,
+      youtube: youtube !== "" ? youtube : userInfo[0]?.data?.youtube,
+      discord: discord !== "" ? discord : userInfo[0]?.data?.discord,
+    };
+
+    updateUser(updatedUserData);
+  };
+
   return (
     <Layouts>
       <div className="primary-content-area container content-padding grid-left-sidebar">
