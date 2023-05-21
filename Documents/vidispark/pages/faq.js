058ecfbaaf405270b894f5f3db27095c3abb0575
mendsalbert <mendsalbert@gmail.com>
2023-05-21 15:42:39 +0000

    vidispark

--- Documents/vidispark/pages/faq.js
+++ Documents/vidispark/pages/faq.js
@@ -25,7 +25,7 @@ const Faq = () => {
                 className="card-button"
               >
                 <div className="card-title">
-                  How can I join Criptoki? Can anyone do it?
+                  How can I join Vidispark? Can anyone do it?
                 </div>
                 <div className="icon-box">
                   <svg
@@ -43,15 +43,8 @@ const Faq = () => {
             </div>
             <Accordion.Collapse eventKey="1st" className="card-body">
               <div className="card-info">
-                A non-fungible token (NFT) is a unit of data stored on a digital
-                ledger, called a blockchain, that certifies a digital asset to
-                be unique and therefore not interchangeable. NFTs can be used to
-                represent items such as photos, videos, audio, and other types
-                of digital files. Access to any copy of the original file,
-                however, is not restricted to the buyer of the NFT. While copies
-                of these digital items are available for anyone to obtain, NFTs
-                are tracked on blockchains to provide the owner with a proof of
-                ownership that is separate from copyright.
+                To join Vidispark, simply visit our website. You can sign up for
+                an account and start exploring the platform.
               </div>
             </Accordion.Collapse>
           </div>
