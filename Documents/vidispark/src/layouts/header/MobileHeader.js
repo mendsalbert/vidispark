8eb61e6d9af8d367847479caf5632f581b66ad98
mendsalbert <mendsalbert@gmail.com>
2023-05-21 07:14:26 +0000

    vidispark

--- Documents/vidispark/src/layouts/header/MobileHeader.js
+++ Documents/vidispark/src/layouts/header/MobileHeader.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Fragment, useEffect, useState } from "react";
 import useClickOutside from "../../useClickOutside";
+import { SparklesIcon } from "@heroicons/react/24/outline";
 
 const MobileHeader = () => {
   const [toggle, setToggle] = useState(false);
@@ -51,344 +52,45 @@ const MobileHeader = () => {
                 <a className="menu-link">Home</a>
               </Link>{" "}
             </li>
-            <li className="menu-item menu-item-has-children">
-              <a
-                className="menu-link"
-                href="#"
-                onClick={() => activeMenuSet("Explore")}
-              >
-                Explore
-                <span className="indicator">
-                  <svg className="crumina-icon">
-                    <use xlinkHref="#arrow-down-icon" />
-                  </svg>
-                </span>
-              </a>
-              <ul className={`submenu ${activeLi("Explore")}`}>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="/02-explore">
-                    <a className="menu-link">Explore</a>
-                  </Link>{" "}
-                </li>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="03-explore-v2">
-                    <a className="menu-link">Explore v2</a>
-                  </Link>{" "}
-                </li>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="04-item-versions">
-                    <a className="menu-link">Item versions</a>
-                  </Link>{" "}
-                </li>
-              </ul>
-            </li>
             <li className="menu-item">
               {" "}
-              <Link href="10-creators">
+              <Link href="/explore">
+                <a className="menu-link"> Explore</a>
+              </Link>{" "}
+            </li>
+
+            <li className="menu-item">
+              {" "}
+              <Link href="/creators">
                 <a className="menu-link">Creators</a>
               </Link>{" "}
             </li>
             <li className="menu-item">
               {" "}
-              <Link href="11-collectors">
-                <a className="menu-link">Collectors</a>
+              <Link href="/marketplace">
+                <a className="menu-link">Marketplace</a>
               </Link>{" "}
             </li>
             <li className="menu-item">
               {" "}
-              <Link href="16-spotlight">
-                <a className="menu-link">Spotlight</a>
+              <Link href="/leaderboard">
+                <a className="menu-link">Leaderboard</a>
               </Link>{" "}
-            </li>
-            <li className="menu-item menu-item-has-children">
-              <a
-                className="menu-link"
-                href="#"
-                onClick={() => activeMenuSet("Product")}
-              >
-                Product
-                <span className="indicator">
-                  <svg className="crumina-icon">
-                    <use xlinkHref="#arrow-down-icon" />
-                  </svg>
-                </span>
-              </a>
-              <ul className={`submenu ${activeLi("Product")}`}>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="05-product-page">
-                    <a className="menu-link">Product v1</a>
-                  </Link>{" "}
-                </li>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="06-product-page- v2">
-                    <a className="menu-link">Product v2</a>
-                  </Link>{" "}
-                </li>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="07-product-page-v3">
-                    <a className="menu-link">Product v3</a>
-                  </Link>{" "}
-                </li>
-              </ul>
-            </li>
-            <li className="menu-item menu-item-has-megamenu menu-item-has-children">
-              <a
-                className="menu-link"
-                href="#"
-                onClick={() => activeMenuSet("pages")}
-              >
-                All pages
-                <span className="indicator">
-                  <svg className="crumina-icon">
-                    <use xlinkHref="#arrow-down-icon" />
-                  </svg>
-                </span>
-                <span className="highlighted-item gradient-background">
-                  NEW
-                </span>
-              </a>
-              <div className={`megamenu ${activeLi("pages")}`}>
-                <div className="megamenu-wrapper">
-                  <div className="megamenu-column">
-                    <div className="megamenu-title">Main Pages</div>
-                    <div>
-                      <ul className="submenu-column">
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="/">
-                            <a className="menu-link">Home</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="02-explore">
-                            <a className="menu-link">Explore</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="03-explore-v2">
-                            <a className="menu-link">Explore v2</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="04-item-versions">
-                            <a className="menu-link">Item versions</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="16-spotlight">
-                            <a className="menu-link">Spotlight</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="05-product-page">
-                            <a className="menu-link">Product page</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="06-product-page-v2">
-                            <a className="menu-link">Product page v2</a>
-                          </Link>{" "}
-                        </li>
-                      </ul>
-                      <ul className="submenu-column">
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="07-product-page-v3">
-                            <a className="menu-link">Product page v3</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="12-blog-grid">
-                            <a className="menu-link">Blog grid</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="13-blog-classic">
-                            <a className="menu-link">Blog classic</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="14-blog-post-centered">
-                            <a className="menu-link">Blog postcentered</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="15-blog-post-sidebar">
-                            <a className="menu-link">Blog post sidebar</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="19-faq">
-                            <a className="menu-link">FAQ</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="20-contact">
-                            <a className="menu-link">Contact</a>
-                          </Link>{" "}
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="megamenu-column">
-                    <div className="megamenu-title">User pages</div>
-                    <div>
-                      <ul className="submenu-column">
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="08-profile-page">
-                            <a className="menu-link">Profile page</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          <a href="09-profile-page-about" className="menu-link">
-                            Profile page about
-                          </a>
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="23-profile-info">
-                            <a className="menu-link">Profile info</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="26-upload-artwork">
-                            <a className="menu-link">Upload artwork</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="25-notifications">
-                            <a className="menu-link">Notifications</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="27-shopping-cart">
-                            <a className="menu-link">Shopping cart</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="28-checkout">
-                            <a className="menu-link">Checkout</a>
-                          </Link>{" "}
-                        </li>
-                      </ul>
-                      <ul className="submenu-column">
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="17-activity">
-                            <a className="menu-link">Activity</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="10-creators">
-                            <a className="menu-link">Creators</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="11-collectors">
-                            <a className="menu-link">Collectors</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          <a href="24-become-a-creator" className="menu-link">
-                            Become a creator
-                          </a>
-                        </li>
-                        <li className="menu-item">
-                          <a href="18-connect-a-wallet" className="menu-link">
-                            Connect a wallet
-                          </a>
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="21-login">
-                            <a className="menu-link">Login</a>
-                          </Link>{" "}
-                        </li>
-                        <li className="menu-item">
-                          {" "}
-                          <Link href="22-register">
-                            <a className="menu-link">Register</a>
-                          </Link>{" "}
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li className="menu-item menu-item-has-children">
-              <a
-                className="menu-link"
-                href="#"
-                onClick={() => activeMenuSet("Blog")}
-              >
-                Blog
-                <span className="indicator">
-                  <svg className="crumina-icon">
-                    <use xlinkHref="#arrow-down-icon" />
-                  </svg>
-                </span>
-              </a>
-              <ul className={`submenu ${activeLi("Blog")}`}>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="12-blog-grid">
-                    <a className="menu-link">Blog grid</a>
-                  </Link>{" "}
-                </li>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="13-blog-classic">
-                    <a className="menu-link">Blog classic</a>
-                  </Link>{" "}
-                </li>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="14-blog-post-centered">
-                    <a className="menu-link">Blog post centered</a>
-                  </Link>{" "}
-                </li>
-                <li className="menu-item">
-                  {" "}
-                  <Link href="15-blog-post-sidebar">
-                    <a className="menu-link">Blog post sidebar</a>
-                  </Link>{" "}
-                </li>
-              </ul>
             </li>
             <li className="menu-item">
               {" "}
-              <Link href="/08-profile-page">
-                <a className="menu-link">
-                  My Collection
-                  <span className="count">12</span>
-                </a>
+              <Link href="/metaverse">
+                <a className="menu-link">Metaverse</a>
               </Link>{" "}
+            </li>
+            <li className="menu-item">
+              {" "}
+              <Link href="/vidisAI">
+                <a className="menu-link">VidisAI</a>
+              </Link>{" "}
+              <span className="highlighted-item gradient-background">
+                <SparklesIcon />
+              </span>
             </li>
           </ul>
         </div>
