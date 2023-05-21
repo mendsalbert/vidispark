60c9dc226f85976bd63f067f6e67b48e6fe6b9b8
mendsalbert <mendsalbert@gmail.com>
2023-05-21 23:20:33 +0000

    vidispark

--- Documents/vidispark/pages/white-paper.js
+++ Documents/vidispark/pages/white-paper.js
@@ -3,10 +3,6 @@ import { Accordion } from "react-bootstrap";
 import Layouts from "../src/layouts/Layouts";
 
 const Faq = () => {
-  const [activeFaq, setActiveFaq] = useState("1st");
-  const onClick = (value) =>
-      activeFaq === value ? setActiveFaq("") : setActiveFaq(value),
-    classNameChange = (value) => (value === activeFaq ? "" : "show");
   return (
     <Layouts>
       <embed
