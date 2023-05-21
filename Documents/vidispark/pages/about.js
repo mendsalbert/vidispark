c5e57f9540d0963a50ff557fa72dfa78184b9912
mendsalbert <mendsalbert@gmail.com>
2023-05-21 16:27:18 +0000

    vidispark

--- Documents/vidispark/pages/about.js
+++ Documents/vidispark/pages/about.js
@@ -81,7 +81,7 @@ const Faq = () => {
                     <a href="#" class="tw-mx-auto">
                       <img
                         class="tw-rounded-2xl tw-drop-shadow-md tw-hover:drop-shadow-xl tw-transition-all tw-duration-200 tw-delay-100"
-                        src="/img/eva.jpg"
+                        src="/img/eva.png"
                       />
                     </a>
 
