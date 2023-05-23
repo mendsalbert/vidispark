bd3f03a0cfa0a18ea95a5190a1d3f96d8939bea8
mendsalbert <mendsalbert@gmail.com>
2023-05-23 20:04:51 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -5,7 +5,6 @@ import { useVideo } from "../../../services/videos";
 import { ToastContainer, toast } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
-import { IconCopy } from "@tabler/icons";
 const ProfileUploadAndPreview = () => {
   const notify = () => toast.success("Video Uploaded Successfully");
   const notify2 = () =>
@@ -223,7 +222,6 @@ const ProfileUploadAndPreview = () => {
               {showKey && (
                 <>
                   Stream Key - {streamKey && streamKey.substring(0, 5) + "..."}{" "}
-                  <IconCopy />
                   <button onClick={copyToClipboard}>Copy API Key</button>
                 </>
               )}
