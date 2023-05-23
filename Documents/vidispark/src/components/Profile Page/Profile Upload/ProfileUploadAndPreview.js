e1b5c11f77504b97b96841f518eced8cd0d05aea
mendsalbert <mendsalbert@gmail.com>
2023-05-23 20:08:20 +0000

    vidispark

--- Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
+++ Documents/vidispark/src/components/Profile Page/Profile Upload/ProfileUploadAndPreview.js
@@ -233,6 +233,7 @@ const ProfileUploadAndPreview = () => {
                       Stream Key -{" "}
                       {streamKey && streamKey.substring(0, 5) + "..."}{" "}
                       <svg
+                        onClick={copyToClipboard}
                         xmlns="http://www.w3.org/2000/svg"
                         class="icon icon-tabler icon-tabler-copy"
                         width="24"
@@ -252,7 +253,6 @@ const ProfileUploadAndPreview = () => {
                         <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
                         <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
                       </svg>
-                      <button onClick={copyToClipboard}>Copy API Key</button>
                     </>
                   )}
                   <br></br>
